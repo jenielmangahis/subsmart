611076ffb8cbe9e3af95f05ffe5f9a9cf6204615
Charles Jayson Bio <mail.bisqy@gmail.com>
2020-06-12 16:26:44 +0800

    contact slider fix

--- application/views/contact.php
+++ application/views/contact.php
@@ -6,19 +6,19 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <section class="contact-spacing">
     <div class="container spacing-ft">
         <div class="row container-contact">
-            <div class="col-sm-6 pt-1 mt-3">
+            <div class="col-sm-5 pt-1 mt-3 mobile-width-100">
               <div class="contact_container">
                 <h2 class="contact-header">Get in touch</h2>
                 <h2 class="uppercase cn-tn">nSmarTrac Pvt. Ltd.</h2>
                 <br/>
-                <h4 class="cn-address">Address content here - lorem ipsum dolor</h4>
+                <h4 class="cn-address">6866 Pine Forest Road <br/> Florida Headquarters <br/> Pensacola, FL 32526</h4>
                 <br/>
                 <div data-container="details" class="sp-ui">
                   <div class="sp-r contact-txt mb-15">
-                    <span class="fa fa-phone"></span><span class="sp-txt ml-20">977.9851005280</span>
+                    <span class="fa fa-phone"></span><span class="sp-txt ml-20">(844) 406-7286</span>
                   </div>
                   <div class="sp-r contact-txt mb-15">
-                    <span class="fa fa-envelope"></span><a href="mailto:info@nsmartrac.com" class="contact-email">info@nsmartrac.com</a>
+                    <span class="fa fa-envelope"></span><a href="mailto:support@nsmartrac.com" class="contact-email">support@nsmartrac.com</a>
                   </div>
                 </div>
                 <br/>
@@ -29,15 +29,15 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
                 </div>
               </div>
             </div>
-            <div class="col-sm-6">
+            <div class="col-sm-7 mobile-width-100">
               <div class="bg-contact-phone">
                 <div class="inquiry-container">
                   <p class="phone-header">Drop us a line</p>
-                  <input type="text" value="" placeholder="Name" class="phone-input"/>
-                  <input type="text" value="" placeholder="Address" class="phone-input"/>
-                  <input type="text" value="" placeholder="Phone number" class="phone-input"/>
-                  <input type="text" value="" placeholder="Email" class="phone-input"/>
-                  <textarea placeholder="Message" class="phone-input" style="height: 95px;"></textarea>
+                  <input type="text" value="" placeholder="Name" class="phone-input-contact"/>
+                  <input type="text" value="" placeholder="Address" class="phone-input-contact"/>
+                  <input type="text" value="" placeholder="Phone number" class="phone-input-contact"/>
+                  <input type="text" value="" placeholder="Email" class="phone-input-contact"/>
+                  <textarea placeholder="Message" class="phone-input-contact" style="height: 95px;"></textarea>
                   <button type="submit" class="btn-phone">Send</button
                 </div>
               </div>
