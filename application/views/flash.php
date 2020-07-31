5d215eb5e9118417b7f1fd32395682309218997a
Jeniel Mangahis <jeniel.mangahis.work@gmail.com>
2020-07-31 23:55:42 +0800

    Added margin in flash container

--- application/views/flash.php
+++ application/views/flash.php
@@ -1,5 +1,5 @@
 <?php if( $this->session->flashdata('message') != '' ){ ?>
-<div class="alert <?php echo $this->session->flashdata('alert_class'); ?> alert-dismissible fade show" style="width: 100%;">
+<div class="alert <?php echo $this->session->flashdata('alert_class'); ?> alert-dismissible fade show" style="width: 100%;margin-top: 10px;margin-bottom: 10px;">
   <p><?php echo $this->session->flashdata('message'); ?></p>
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">×</span>
