a01bae92aa39b1ffa35fa54bf271c2a42ac40e82
Bryann Revina <bryann.revina03@gmail.com>
2020-09-17 19:10:45 +0800

    Updates  - Initial Layout for Marketing -> Campaign Blast     - https://prnt.sc/uiupdx     - https://prnt.sc/uiuzsv

---
+++ application/views/includes/campaignblast_modals.php
@@ -0,0 +1,132 @@
+<!-- Modal Add Campaign Blast -->
+<div class="modal fade bd-example-modal-lg" id="modalAddCampaignBlast" tabindex="-1" role="dialog" aria-labelledby="modalAddCampaignBlastmTitle" aria-hidden="true">
+  <?php echo form_open_multipart('booking/create_service_item', [ 'class' => 'form-validate', 'autocomplete' => 'off' ]); ?>
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Add Campaign Blast</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form data-form="form" method="post" action="#">
+                
+            <div class="card">
+                <div class="row margin-bottom">
+                    <div class="col-sm-18 col-xl-12">
+                        <label>Campaign Name</label>
+                        <input type="text" name="name" value="" class="form-control">
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-18 col-xl-12">
+                        <label>Postcard Return Address</label>
+                        <div class="help help-sm margin-bottom-sec">Complete the fields below to return the postcard if it can not be delivered.</div>
+
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <label>Return to Name/Company</label>
+                                    <input type="text" name="postcard_return_address_name" value="" id="postcard_return_address_name" class="form-control" autocomplete="off">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <label>Address</label> <span class="help help-sm">(type in to search for address)</span>
+                                    <input type="text" name="postcard_return_address_address" value="" id="postcard_return_address_address" class="form-control pac-target-input" autocomplete="off" placeholder="e.g. 123 Old Oak Drive">
+                                    <input name="postcard_return_address_latlng" id="postcard_return_address_latlng" type="hidden" value="">
+                                </div>
+                                <div class="col-sm-12">
+                                    <label>Suite/Unit</label>
+                                    <input type="text" name="postcard_return_address_address_secondary" value="" class="form-control" autocomplete="off">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-7">
+                                <label>City </label>
+                                <input type="text" name="postcard_return_address_city" value="" id="postcard_return_address_city" class="form-control" autocomplete="off">
+                            </div>
+                            <div class="col-sm-5">
+                                <label>Zip/Postal Code</label>
+                                <input type="text" name="postcard_return_address_zip" value="" id="postcard_return_address_zip" class="form-control" autocomplete="off">
+                            </div>
+                            <div class="col-sm-12">
+                                <label>State/Province</label>
+                                <select name="postcard_return_address_state" id="postcard_return_address_state" class="form-control">
+                                  <option value="" selected="selected">- select -</option>
+                                  <option value="AK">Alaska</option>
+                                  <option value="AL">Alabama</option>
+                                  <option value="AR">Arkansas</option>
+                                  <option value="AZ">Arizona</option>
+                                  <option value="CA">California</option>
+                                  <option value="CO">Colorado</option>
+                                  <option value="CT">Connecticut</option>
+                                  <option value="DC">District of Columbia</option>
+                                  <option value="DE">Delaware</option>
+                                  <option value="FL">Florida</option>
+                                  <option value="GA">Georgia</option>
+                                  <option value="HI">Hawaii</option>
+                                  <option value="IA">Iowa</option>
+                                  <option value="ID">Idaho</option>
+                                  <option value="IL">Illinois</option>
+                                  <option value="IN">Indiana</option>
+                                  <option value="KS">Kansas</option>
+                                  <option value="KY">Kentucky</option>
+                                  <option value="LA">Louisiana</option>
+                                  <option value="MA">Massachusetts</option>
+                                  <option value="MD">Maryland</option>
+                                  <option value="ME">Maine</option>
+                                  <option value="MI">Michigan</option>
+                                  <option value="MN">Minnesota</option>
+                                  <option value="MO">Missouri</option>
+                                  <option value="MS">Mississippi</option>
+                                  <option value="MT">Montana</option>
+                                  <option value="NC">North Carolina</option>
+                                  <option value="ND">North Dakota</option>
+                                  <option value="NE">Nebraska</option>
+                                  <option value="NH">New Hampshire</option>
+                                  <option value="NJ">New Jersey</option>
+                                  <option value="NM">New Mexico</option>
+                                  <option value="NV">Nevada</option>
+                                  <option value="NY">New York</option>
+                                  <option value="OH">Ohio</option>
+                                  <option value="OK">Oklahoma</option>
+                                  <option value="OR">Oregon</option>
+                                  <option value="PA">Pennsylvania</option>
+                                  <option value="RI">Rhode Island</option>
+                                  <option value="SC">South Carolina</option>
+                                  <option value="SD">South Dakota</option>
+                                  <option value="TN">Tennessee</option>
+                                  <option value="TX">Texas</option>
+                                  <option value="UT">Utah</option>
+                                  <option value="VA">Virginia</option>
+                                  <option value="VT">Vermont</option>
+                                  <option value="WA">Washington</option>
+                                  <option value="WI">Wisconsin</option>
+                                  <option value="WV">West Virginia</option>
+                                  <option value="WY">Wyoming</option>
+                                </select>
+                                <input type="hidden" name="postcard_return_address_country" value="us" id="postcard_return_address_country">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr class="card-hr" >
+            <button type="button" style="float: right;" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Add</button>            
+        </form>     		
+      </div>
+      </div>
+      <div class="modal-footer">
+        
+      </div>
+    </div>
+  </div>
+  <?php echo form_close(); ?>
+</div>
--- application/views/includes/sidebars/marketing.php
+++ application/views/includes/sidebars/marketing.php
@@ -31,7 +31,7 @@
         <li class="submenus <?= ($this->uri->uri_string() == 'email_campaigns') ? "active" : "";  ?>"><a href="<?php echo base_url('email_campaigns'); ?>" title="Email Blast"><span class="fa fa-cube"></span>Email Blast</a></li>
         <li class="submenus <?= ($this->uri->uri_string() == 'email_automation') ? "active" : "";  ?>"><a href="email_automation" title="Email Automation"><span class="fa fa-cube"></span>Email Automation</a></li>
         <li class="submenus <?= ($this->uri->uri_string() == 'offers') ? "active" : "";  ?>"><a href="<?php echo base_url('offers'); ?>" title="Offers"><span class="fa fa-cube"></span>Offers</a></li>
-        <li class="submenus"><a href="#" title="Credentials"><span class="fa fa-cube"></span>Campaign Blast</a></li>
+        <li class="submenus  <?= ($this->uri->uri_string() == 'campaign_blast') ? "active" : "";  ?>"><a href="campaign_blast" title="Credentials"><span class="fa fa-cube"></span>Campaign Blast</a></li>
         <li class="submenus"><a href="#" title="Credentials"><span class="fa fa-cube"></span>Customer Finder 360</a></li>
         <li class="submenus"><a href="#" title="Credentials"><span class="fa fa-cube"></span>My Leads List</a></li>
 
