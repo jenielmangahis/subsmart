76283a49552e6c9421b02fd8b7090021c966d895
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2020-10-20 19:02:28 +0800

    Updates   - Industry Layout Template - Industry Template : https://prnt.sc/v2zm44       - Added create template       - Added edit template       - Added delete template       - Added list for industry template    - Added it on the plan builder side menu.

---
+++ application/views/industry_template/index.php
@@ -0,0 +1,87 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<?php include viewPath('includes/header_plan_builder'); ?>
+<style>
+.cell-active{
+    background-color: #5bc0de;
+}
+.cell-inactive{
+    background-color: #d9534f;
+}
+</style>
+<div class="wrapper" role="wrapper">
+    <?php include viewPath('includes/sidebars/nsmart_plans'); ?>
+    <!-- page wrapper start -->
+    <div wrapper__section>
+        <div class="container-fluid">
+            <div class="page-title-box">
+                <div class="row align-items-center">
+                    <div class="col-sm-6">
+                        <h1 class="page-title">Industry Template</h1>
+                    </div>
+                </div>
+            </div>
+            <!-- end row -->
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="card" style="min-height: 400px !important;">
+                        <div class="row dashboard-container-1">
+                            <div class="col-md-12 text-right">
+                                <a class="btn btn-info" href="<?php echo base_url('industry_template/add_new_template'); ?>"><i class="fa fa-file"></i> New Industry Template</a>
+                            </div>
+                        </div>       
+                        <hr />
+                        <?php include viewPath('flash'); ?>
+                        <table class="table table-hover" data-id="coupons">
+                            <thead>
+                                <tr>
+                                    <th style="width: 20%;">Industry Template Name</th>
+                                    <th style="width: 15%;">Status</th>
+                                    <th style="width: 15%;"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach( $industryTemplate as $indTemplate){ ?>
+                                    <?php 
+                                        if( $indTemplate->status == 1 ){
+                                            $cell = 'cell-active';
+                                            $status = "Active";
+                                        }else{
+                                            $cell = 'cell-inactive';
+                                            $status = "Inactive";
+                                        }
+                                    ?>
+                                    <tr>
+                                        <td><?= $indTemplate->name; ?></td>
+                                        <td class="<?= $cell; ?>"><?= $status; ?></td>
+                                        <td>
+                                            <a class="btn btn-info btn-sm" href="<?php echo base_url('industry_template/edit_template/'.$indTemplate->id); ?>"><i class="fa fa-pencil"></i> Edit</a>
+                                            <a class="btn btn-sm btn-danger btn-delete-template" href="javascript:void(0);" data-id="<?= $indTemplate->id; ?>"><i class="fa fa-trash"></i> Delete</a>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- end card -->
+                </div>
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container-fluid -->
+    </div>
+    <!-- page wrapper end -->
+</div>
+<?php include viewPath('includes/plan_builder_modals'); ?> 
+<?php include viewPath('includes/footer_plan_builder'); ?>
+
+<script type="text/javascript">  
+$(function(){
+    $(".btn-delete-template").click(function(){
+        var template_id = $(this).attr("data-id");
+        $("#tid").val(template_id);
+
+        $("#modalDeleteIndustryTemplate").modal("show");
+    });
+});
+</script>
