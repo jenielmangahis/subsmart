f337ab63dff8e540431fde20ade8fa945b0a9cbf
Charles Jayson Bio <mail.bisqy@gmail.com>
2021-03-03 14:56:01 +0800

    fixed layout edit event

--- application/views/event_types/edit.php
+++ application/views/event_types/edit.php
@@ -5,13 +5,35 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 .marker-icon{
   height: 100px;
   margin: 30px 0px;
-} 
+}
+.page-title {
+  font-family: Sarabun, sans-serif !important;
+  font-size: 1.75rem !important;
+  font-weight: 600 !important;
+}
+.pr-b10 {
+  position: relative;
+  bottom: 10px;
+}
+.p-40 {
+  padding-top: 40px !important;
+}
+@media only screen and (max-width: 600px) {
+  .p-40 {
+    padding-top: 0px !important;
+  }
+  .pr-b10 {
+    position: relative;
+    bottom: 0px;
+  }
+}
 </style>
 <div class="wrapper" role="wrapper">
     <?php include viewPath('includes/sidebars/events'); ?>
     <!-- page wrapper start -->
     <div wrapper__section>
-        <div class="container-fluid">
+        <div class="container-fluid p-40">
+          <!--
             <div class="page-title-box">
                 <div class="row align-items-center">
                     <div class="col-sm-6">
@@ -19,10 +41,12 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
                     </div>
                 </div>
             </div>
+          -->
             <!-- end row -->
             <div class="row">
                 <div class="col-xl-12">
-                    <div class="card" style="min-height: 400px !important;">
+                    <div class="card p-20" style="min-height: 400px !important;">
+                        <h3 class="page-title mb-0"><i class="fa fa-edit"></i> Edit Event Type</h3>
                         <hr />
                         <?php include viewPath('flash'); ?>
                         <?php echo form_open_multipart('event_types/update', [ 'class' => 'form-validate', 'autocomplete' => 'off' ]); ?>
@@ -32,14 +56,14 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
                               <input type="text" name="title" value="<?= $eventType->title; ?>"  class="form-control" required="" autocomplete="off" />
                           </div>
                           <div class="form-group">
-                              <?php 
+                              <?php
                                 if( $eventType->icon_marker != '' ){
                                   $url = base_url('uploads/event_types/' . $eventType->company_id . '/' . $eventType->icon_marker);
                                 }else{
                                   $url = base_url('uploads/event_types/no_file.png');
                                 }
                               ?>
-                              
+
                               <label>Icon / Marker</label> <span class="form-required"></span>
                               <img src="<?= $url; ?>" class="marker-icon" />
                               <input type="file" name="image" value=""  class="form-control"  autocomplete="off" />
