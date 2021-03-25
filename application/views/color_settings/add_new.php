34491fa55041f480823c57a2a7d27faf2a808171
bio <mail.bisqy@gmail.com>
2021-03-25 13:17:45 +0800

    fixed add new color

--- application/views/color_settings/add_new.php
+++ application/views/color_settings/add_new.php
@@ -1,23 +1,56 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <?php include viewPath('includes/header'); ?>
+<style>
+.page-title {
+  font-family: Sarabun, sans-serif !important;
+  font-size: 1.75rem !important;
+  font-weight: 600 !important;
+}
+.pr-b10 {
+  position: relative;
+  bottom: 10px;
+}
+.p-40 {
+  padding-top: 40px !important;
+}
+@media only screen and (max-width: 600px) {
+  .p-40 {
+    padding-top: 0px !important;
+  }
+  .pr-b10 {
+    position: relative;
+    bottom: 0px;
+  }
+}
+.list-icon{
+  list-style: none;
+  height: 400px;
+  overflow: auto;
+  padding: 6px;
+}
+.list-icon li{
+  display: inline-block;
+  /*width: 30%;*/
+  height:100px;
+  margin: 3px;
+}
+.icon-image{
+  height: 50px;
+  width: 50px;
+}
+</style>
 <div class="wrapper" role="wrapper">
     <?php include viewPath('includes/sidebars/schedule'); ?>
     <!-- page wrapper start -->
     <div wrapper__section>
-        <div class="container-fluid">
-            <div class="page-title-box">
-                <div class="row align-items-center">
-                    <div class="col-sm-6">
-                        <h1 class="page-title"><i class="fa fa-plus"></i> Add New Color</h1>
-                    </div>
-                </div>
-            </div>
+        <div class="container-fluid p-40">
             <!-- end row -->
             <div class="row">
                 <div class="col-xl-12">
-                    <div class="card" style="min-height: 400px !important;">
-                        <hr />
+                    <div class="card mt-0" style="min-height: 400px !important;">
+                        <h3 class="page-title mb-0"><i class="fa fa-plus"></i> Add New Color</h3>
+                        <hr/>
                         <?php include viewPath('flash'); ?>
                         <?php echo form_open_multipart('color_settings/create_color_setting', [ 'class' => 'form-validate', 'autocomplete' => 'off' ]); ?>
 
