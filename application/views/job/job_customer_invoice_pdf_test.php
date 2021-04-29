61ab646d97d62305d774f0be783068c683cf04ca
Jeniel Mangahis <jeniel.mangahis.work@gmail.com>
2021-04-29 13:58:26 +0800

    Added test pdf file

---
+++ application/views/job/job_customer_invoice_pdf_test.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+
+<table style="width: 100%;font-size: 10px;">
+    <tr>
+        <td style="text-align: right;" colspan=2><span style="font-weight: 700;font-size: 16px;">test</span><br></td>
+    </tr>
+</table>
