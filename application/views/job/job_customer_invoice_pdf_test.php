76eeab7c5f387c3c8e7fc9443b4bd7364deaca23
bio <mail.bisqy@gmail.com>
2021-04-30 15:04:41 +0800

    added pdf ui

--- application/views/job/job_customer_invoice_pdf_test.php
+++ application/views/job/job_customer_invoice_pdf_test.php
@@ -2,8 +2,302 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
-<table style="width: 100%;font-size: 10px;">
+<table style="font-size: 10px;" width="100%">
     <tr>
-        <td style="text-align: right;" colspan=2><span style="font-weight: 700;font-size: 16px;">test</span><br></td>
+        <td width="40%">
+
+            <table style="font-size: 10px;" width="100%">
+                <tr>
+                    <td width="70%">company image here</td>
+                </tr>
+                <tr>
+                    <td width="70%"></td>
+                </tr>
+                <tr>
+                    <td width="70%"></td>
+                </tr>
+                <tr>
+                    <td width="70%"></td>
+                </tr>
+                <tr>
+                    <td width="100%">P.O BOX 731340</td>
+                </tr>
+                <tr>
+                    <td width="100%">DALLAS, TX 75373-1340</td>
+                </tr>
+                <tr>
+                    <td width="70%"></td>
+                </tr>
+                <tr>
+                    <td width="70%"></td>
+                </tr>
+                <tr>
+                    <td width="100%">ADI SMART HOME</td>
+                </tr>
+                <tr>
+                    <td width="100%">9175 KINGS COLONY ROAD</td>
+                </tr>
+                <tr>
+                    <td width="100%">JACKSONVILLE, FL 32257</td>
+                </tr>
+            </table>
+        </td>
+        <td width="60%">
+            <table style="width: 100%;font-size: 10px;">
+                <tr>
+                    <td width="50%" style="padding:0px;">
+                        <table>
+                            <tr>
+                                <td width="100%" style="text-align:left;page-break-after: always;display:block;font-weight:bold;font-size:13px;">ORIGINAL INVOICE</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:left;display:block;font-size:9px;">CUSTOMER INVOICE</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:left;display:block;font-size:9px;">PLEASE WRITE THIS NUMBER</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:left;display:block;font-size:9px;">ON ALL ORDERS AND CHECKS</td>
+                            </tr>
+                        </table>
+                        <table>
+                            <tr>
+                                <td width="100%" style="page-break-after: always;display:block;border-collapse: collapse;border:1px solid black;text-align:center;">
+                                <table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">DP311-000</span>
+                                </td>    
+                            </tr>
+                        </table>
+                        <table>
+                            <tr>
+                                <td style="text-align:left;display:block;font-size:15px;"></td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:right;display:block;font-size:9px;font-weight:bold;">PLEASE PAY THIS AMOUNT</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:right;display:block;font-size:8px;"></td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:right;display:block;font-size:6px;"></td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:right;display:block;font-size:9px;font-weight:bold;">DUE DATE</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:right;display:block;font-size:9px;"></td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:right;display:block;font-size:4px;"></td>
+                            </tr>
+                            <tr>
+                                <td width="100%" style="text-align:left;page-break-after: always;display:block;font-weight:bold;font-size:13px;">REMIT TO:</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:left;display:block;font-size:9px;">SUBSMART INC., DBS ADI</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:left;display:block;font-size:9px;">PO BOX 32422</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:left;display:block;font-size:9px;">DALLAS TEXAS</td>
+                            </tr>
+                        </table>
+                    </td>
+                    <td width="50%">
+                        <table>
+                            <tr>
+                                <td width="40%" style="text-align:center;page-break-after: always;display:block;border:1px solid black;font-weight:bold;">PAGE</td>
+                                <td width="60%" style="text-align:center;page-break-after: always;display:block;border:1px solid black;font-weight:bold;">INVOICE DATE</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:center;page-break-after: always;display:block;border:1px solid black;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">1</span></td>
+                                <td style="text-align:center;display:block;border:1px solid black;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">04/27/2021</span></td>
+                            </tr>
+                            <tr><td style="font-size:4px;"></td></tr>
+                            <tr>
+                                <td width="100%" style="text-align:center;page-break-after: always;display:block;border:1px solid black;font-weight:bold;">INVOICE NUMBER</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:center;page-break-after: always;display:block;border:1px solid black;">
+                                <table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">WW283728</span></td>
+                            </tr>
+                            <tr><td style="font-size:4px;"></td></tr>
+                            <tr>
+                                <td style="text-align:center;page-break-after: always;display:block;border:1px solid black;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">-169.00</span></td>
+                            </tr>
+                            <tr><td style="font-size:4px;"></td></tr>
+                            <tr>
+                                <td style="text-align:center;page-break-after: always;display:block;border:1px solid black;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">04/22/2021</span></td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <table>
+                <tr>
+                    <td style="text-align:right;display:block;font-size:5px;"></td>
+                </tr>
+                <tr>
+                    <td style="text-align:center;display:block;font-size:9px;">TEL: (800) 549-3321 EXT: 5306</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center;display:block;font-size:9px;">FAX: (302) 689-3882</td>
+                </tr>
+
+            </table>
+        </td>
     </tr>
 </table>
+<table>
+    <tr>
+        <td style="text-align:right;display:block;font-size:13px;"></td>
+    </tr>
+</table>
+<table style="font-size: 10px;" width="100%">
+    <tr>
+        <td width="50%" style="text-align:center;display:block;font-size:9px;font-weight:bold;">Please detach and enclose top portion with your payment</td>
+        <td width="50%" style="text-align:center;display:block;font-size:9px;font-weight:bold;">Make check payable and remit to above address</td>
+    </tr>
+</table>
+<table style="font-size: 10px;" width="100%">
+    <tr>
+        <td style="text-align:right;display:block;font-size:3px;"></td>
+    </tr>
+    <tr>
+        <td style="border-top:1px dashed black;"></td>
+    </tr>
+</table>
+<br/>
+
+<table style="font-size: 10px;" width="100%">
+    <tr>
+        <td width="20%">
+            company image here
+        </td>
+        <td width="20%">
+            <table>
+                <tr>
+                    <td width="100%" style="text-align:center;page-break-after: always;display:block;font-weight:bold;font-size:9px;">CUSTOMER NO#</td>
+                </tr>
+                <tr>
+                    <td width="100%" style="text-align:center;page-break-after: always;display:block;border:1px solid black;font-weight:bold;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">DP-311-00</span></td>
+                </tr>
+            </table>
+        </td>
+        <td width="20%">
+            <table>
+                <tr>
+                    <td width="100%" style="text-align:center;page-break-after: always;display:block;font-weight:bold;font-size:9px;">INVOICE NO#</td>
+                </tr>
+                <tr>
+                    <td width="100%" style="text-align:center;page-break-after: always;display:block;border:1px solid black;font-weight:bold;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">WW82VF021</span></td>
+                </tr>
+            </table>
+        </td>
+        <td width="20%">
+            <table>
+                <tr>
+                    <td width="100%" style="text-align:center;page-break-after: always;display:block;font-weight:bold;font-size:9px;">DATE</td>
+                </tr>
+                <tr>
+                    <td width="100%" style="text-align:center;page-break-after: always;display:block;border:1px solid black;font-weight:bold;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">04/27/21</span></td>
+                </tr>
+            </table>
+        </td>
+        <td width="20%">
+            <table>
+                <tr>
+                    <td width="100%" style="text-align:center;page-break-after: always;display:block;font-weight:bold;font-size:9px;"></td>
+                </tr>
+                <tr>
+                    <td width="100%" style="text-align:left;page-break-after: always;display:block;font-weight:bold;"><span style="width:100%;display:block;text-align:left;">Retain this portion for your records</span></td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+
+<table>
+    <tr>
+        <td style="text-align:right;display:block;font-size:13px;"></td>
+    </tr>
+</table>
+
+<table style="font-size: 10px;" width="100%">
+    <tr>
+        <td width="40%" style="border:1px solid black;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">SHIP DATA/ITEM DESCRIPTION</span></td>
+        <td width="15%" style="border:1px solid black;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">ITEM NUMBER</span></td>
+        <td width="15%" style="border:1px solid black;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">QTY SHIPPED</span></td>
+        <td width="15%" style="border:1px solid black;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">UNIT PRICE</span></td>
+        <td width="15%" style="border:1px solid black;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">AMOUNT</span></td>
+    </tr>
+    <tr>
+        <td width="40%" style="border:1px solid black;height:200px;">SHIP FROM: JACKSON VILLE <br/> TO: LOREM IPSUM <br/> PO: FRT CREDIT WV06NIR <br/> SHIP VIA: GROUND G <br/> ORIGORD=WV06VN01 <br/> SHIPPING CHARGE</td>
+        <td width="15%" style="border:1px solid black;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">WP3210</span></td>
+        <td width="15%" style="border:1px solid black;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">10</span></td>
+        <td width="15%" style="border:1px solid black;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">$100.00</span></td>
+        <td width="15%" style="border:1px solid black;"><table><tr><td></td></tr></table><span style="width:100%;display:block;text-align:center;">$1000.00</span></td>
+    </tr>
+    <tr>
+        <td width="55%"><table><tr><td style="font-size:4px;"></td></tr></table><table><tr><td style="font-size:4px;"></td></tr></table><span style="font-size:8px;">REF VISA #------------5898 169.00</span></td>
+        <td width="30%" style="border:1px solid black;"><table><tr><td style="font-size:4px;"></td></tr></table><span style="width:100%;display:block;text-align:left;font-weight:bold;">TOTAL MATERIAL</span></td>
+        <td width="15%" style="border:1px solid black;"><table><tr><td style="font-size:4px;"></td></tr></table><span style="width:100%;display:block;text-align:right;">.00</span></td>
+    </tr>
+    <tr>
+        <td width="55%"><span style="font-size:8px;">E-CHECK NOW AVAILABLE. CONTACT YOUR ADI CREDIT ANALYST. <br/> SIGN UP FOR E-INVOICING GO TO: ADIGLOBAL.COM/GOGREEN</span></td>
+        <td width="30%" style="border:1px solid black;"><table><tr><td style="font-size:4px;"></td></tr></table><span style="width:100%;display:block;text-align:left;font-weight:bold;">SALES TAX</span></td>
+        <td width="15%" style="border:1px solid black;"><table><tr><td style="font-size:4px;"></td></tr></table><span style="width:100%;display:block;text-align:right;">.00</span></td>
+    </tr>
+    <tr>
+        <td width="55%"></td>
+        <td width="30%" style="border:1px solid black;"><table><tr><td style="font-size:4px;"></td></tr></table><span style="width:100%;display:block;text-align:left;font-weight:bold;">SHIPPING & HANDLING</span></td>
+        <td width="15%" style="border:1px solid black;"><table><tr><td style="font-size:4px;"></td></tr></table><span style="width:100%;display:block;text-align:right;">-169.00</span></td>
+    </tr>
+    <tr>
+        <td width="55%"><table><tr><td style="font-size:4px;"></td></tr></table></td>
+        <td width="30%" style="border:1px solid black;"><table><tr><td style="font-size:4px;"></td></tr></table><span style="width:100%;font-size:13px;display:block;text-align:left;font-weight:bold;">TOTAL INVOICE</span></td>
+        <td width="15%" style="border:1px solid black;"><table><tr><td style="font-size:4px;"></td></tr></table><span style="width:100%;display:block;text-align:right;">-169.00</span></td>
+    </tr>
+    <tr>
+        <td width="55%"><table><tr><td style="font-size:4px;"></td></tr></table></td>
+        <td width="30%" style="border:1px solid black;"><table><tr><td style="font-size:4px;"></td></tr></table><span style="width:100%;font-size:13px;display:block;text-align:left;font-weight:bold;">DUE DATE</span></td>
+        <td width="15%" style="border:1px solid black;"><table><tr><td style="font-size:4px;"></td></tr></table><span style="width:100%;display:block;text-align:right;">04/25/21</span></td>
+    </tr>
+</table>
+
+<table>
+    <tr>
+        <td style="text-align:right;display:block;font-size:5px;"></td>
+    </tr>
+</table>
+
+
+<table style="font-size: 10px;" width="100%">
+    <tr>
+        <td width="25%"><table><tr><td style="font-size:4px;"></td></tr></table><span style="text-align:center;color:red;font-size:12px;font-weight:bold;">THANK YOU FOR YOUR ORDER</td>
+        <td width="50%"><span> </span></td>
+        <td width="15%"><span> </span></td>
+    </tr>
+</table>
+<br/>
+<table style="font-size: 10px;" width="100%">
+    <tr>
+        <td width="100%"><table><tr><td style="font-size:4px;"></td></tr></table><span style="text-align:left;font-size:8px;font-weight:bold;">All claims must be made within 5 days after receipt of goods. Goods returned without our authorized return number on the carton will be refused. The purchase of products and services are subject to and governed solely by the Terms and Conditions.<br/><span style="color:blue;">https://www.adiglobal.com/terms-and-conditions</span> <br/> <span style="color:red;">Past due balances may be subject to a Late Charge not to exceed 1.5% per month.</span></td>
+    </tr>
+</table>
+
+
+
+
+
+<!--
+<table style="width: 100%;font-size: 10px;">
+    <tr>
+        <td>
+            <img style="width: 100px;" alt="Attachment" src="<?= dirname(__DIR__) . '/uploads/users/business_profile/10/1.jpg'; ?>">
+        </td>
+    </tr>
+    <tr>
+        <td style="text-align: right;" colspan=2><span style="font-weight: 700;font-size: 16px;">1</span><br></td>
+    </tr>
+</table> -->
