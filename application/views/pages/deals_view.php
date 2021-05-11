66e1397cbf079d501cc7b088c86931be88496fe5
bio <mail.bisqy@gmail.com>
2021-05-12 00:56:49 +0800

    added ui for business preview

--- application/views/pages/deals_view.php
+++ application/views/pages/deals_view.php
@@ -16,128 +16,156 @@
     margin-right: -15px;
     display: block;
 }
+.share a {
+    font-size: 32px;
+    margin-right: 21px;
+}
+ul.business-info li {
+    font-size: 15px;
+    list-style-type: disc;
+    margin-left: 17px;
+    margin-bottom: 4px;
+}
+hr {
+    margin-top: 1rem !important;
+    margin-bottom: 1rem !important;
+}
+.icon-top {
+    color: #9264b9;
+    font-size: 30px;
+    width: 100%;
+    font-weight: bold;
+}
+ul.deal-stats.clearfix li {
+    width: 33.33%;
+    float: left;
+}
+a.btn-purple {
+    float: right;color: #ffffff;font-weight: bold;background: #64477d;padding: 7px 28px;border-radius: 25px;
+}
+ul.deal-stats.clearfix li span {
+    color: #757171 !important;
+    font-size: 16px;
+    margin-top: 10px;
+}
 </style>
-<div class="container" style="padding-top: 0px;background:white;">
-  <div class="header-cnt">
+<div class="container" style="padding-top: 0px;background:white;padding-left:37px;padding-right:37px;">
+   <div class="header-cnt">
       <div class="btn-back-cnt">
-          <a href="https://www.markate.com/deals/bid/14356">View all my deals</a>
+         <a class="btn-purple" href="https://www.markate.com/deals/bid/14356">View all my deals</a>
       </div>
       <div class="row">
-          <div class="col-sm-18">
-              <h1>DEAL 2021</h1>
-          </div>
+         <div class="col-sm-18">
+            <h1>DEAL 2021</h1>
+         </div>
       </div>
-  </div>
-
-
-  <div class="row">
-        <div class="col-md-6">
-
-            <div class="deal-cnt">
-                <div class="image-carousel">
-    <div class="image-carousel-slides" data-image-carousel="slides" style="height: 447px;">
-        <div class="image-carousel-slide" data-image-carousel="slide">
-            <img src="https://markate.blob.core.windows.net/cdn/20200925/busdeal_1195_ad03392f23_md.jpg">
-        </div>
-        <div class="image-carousel-btn previous" data-image-carousel="previous" style="display: none;">
-            <i class="fa fa-angle-left"></i>
-        </div>
-        <div class="image-carousel-btn next" data-image-carousel="next" style="display: none;">
-            <i class="fa fa-angle-right"></i>
-        </div>
-    </div>
-    </div>
-                <div class="deal-subtitle">What You'll Get</div>
-                <hr>
-                <p class="margin-bottom">THIS IS A 2021 SUPER DEAL</p>
-
-                <div class="deal-subtitle">Terms &amp; Conditions</div>
-                <hr>
-                <p class="margin-bottom">
-                                        this is for the terms and conditions                                    </p>
-
-                            </div>
-
-        </div>
-        <div class="col-md-6">
-
-            <ul class="deal-stats  clearfix">
-                <li>
-                    <i class="fa fa-scissors"></i>
-                    Discount <span class="deal-stats-discount text-ter">50%</span>
-                </li>
-                <li>
-                    <i class="fa fa-calendar-check-o"></i>
-                    <span>Expires in 24 days</span>
-                </li>
-                <li>
-                    <i class="fa fa-eye"></i>
-                    Under 10 viewed
-                </li>
-                            </ul>
-
-            <hr class="deals-stats-hr">
-
-            <div class="text-center margin-bottom-ter">
-                <div class="row">
-                    <div class="col-xs-12 text-right">
-                        <div class="deal-price-cnt">
-                            <div class="deal-price-label text-sec">Deal Price</div>
-                            <div class="deal-price text-sec">
-                                $5.00                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 text-left">
-                        <div class="deal-price-original-cnt">
-                            <div class="deal-price-original-label text-ter">Was</div>
-                            <div class="deal-price-original text-ter">
-                                $10.00                            </div>
-                        </div>
-                    </div>
-                </div>
+   </div>
+   <div class="row">
+      <div class="col-md-6 left pl-0" style="border-right:1px solid #e5e5e5;min-height:807px;">
+         <div class="deal-cnt">
+            <div class="image-carousel">
+               <div class="image-carousel-slides" data-image-carousel="slides" style="height: auto;margin-bottom:40px;">
+                  <div class="image-carousel-slide" data-image-carousel="slide">
+                     <img src="https://markate.blob.core.windows.net/cdn/20200925/busdeal_1195_ad03392f23_md.jpg">
+                  </div>
+                  <div class="image-carousel-btn previous" data-image-carousel="previous" style="display: none;">
+                     <i class="fa fa-angle-left"></i>
+                  </div>
+                  <div class="image-carousel-btn next" data-image-carousel="next" style="display: none;">
+                     <i class="fa fa-angle-right"></i>
+                  </div>
+               </div>
             </div>
-
-            <a class="btn btn-primary btn-lg btn-block" href="https://www.markate.com/deals/book/view/id/1195">Book This Deal</a>
-            <div class="text-center margin-top-sec">
-                <span class="text-ter text-sm">No credit card required</span>
+            <div class="deal-subtitle bold">What You'll Get</div>
+            <hr>
+            <p class="margin-bottom">THIS IS A 2021 SUPER DEAL</p>
+            <div class="deal-subtitle bold">Terms &amp; Conditions</div>
+            <hr>
+            <p class="margin-bottom">
+               this is for the terms and conditions                                    
+            </p>
+         </div>
+      </div>
+      <div class="col-md-6 left pr-4 pl-4">
+         <ul class="deal-stats clearfix">
+            <li>
+               <i class="fa fa-scissors icon-top"></i>
+               <span class="deal-stats-discount text-ter">Discount 50%</span>
+            </li>
+            <li>
+               <i class="fa fa-calendar-check-o icon-top"></i>
+               <span>Expires in 24 days</span>
+            </li>
+            <li>
+               <i class="fa fa-eye icon-top"></i>
+               <span>Under 10 viewed</span>
+            </li>
+         </ul>
+         <hr class="deals-stats-hr">
+         <div class="text-center margin-bottom-ter" style="margin-bottom:50px;">
+            <div class="row">
+               <div class="col-6 left text-right">
+                  <div class="deal-price-cnt">
+                     <div class="deal-price-label text-sec bold">Deal Price</div>
+                     <div class="deal-price text-sec" style="font-size:28px;">
+                        $5.00                            
+                     </div>
+                  </div>
+               </div>
+               <div class="col-6 left text-left">
+                  <div class="deal-price-original-cnt">
+                     <div class="deal-price-original-label text-ter bold">Was</div>
+                     <div class="deal-price-original text-ter" style="margin-top:8px;font-size:20px;text-decoration: line-through">
+                        $10.00                            
+                     </div>
+                  </div>
+               </div>
             </div>
-
-            <hr class="margin-top margin-bottom">
-
-            <div class="side-title">Share This Deal</div>
-            <div class="share">
-    <a class="share-circle share-facebook" href="https://www.facebook.com/dialog/share?app_id=1676920742581843&amp;display=popup&amp;href=https%3A%2F%2Fwww.markate.com%2Fdeal%2Fdeal-2021%2F1195&amp;redirect_uri=https%3A%2F%2Fwww.markate.com%2Fdeal%2Fdeal-2021%2F1195&amp;" target="_blank"><span class="fa fa-facebook"></span></a>
-    <a class="share-circle share-twitter" href="https://twitter.com/share?url=https%3A%2F%2Fwww.markate.com%2Fdeal%2Fdeal-2021%2F1195&amp;via=Markate&amp;text=DEAL+2021&amp;" target="_blank"><span class="fa fa-twitter"></span></a>
-    <a class="share-circle share-google" href="https://plus.google.com/share?url=https%3A%2F%2Fwww.markate.com%2Fdeal%2Fdeal-2021%2F1195&amp;title=DEAL+2021&amp;" target="_blank"><span class="fa fa-google"></span></a>
-    <a class="share-circle share-pinterest" href="http://pinterest.com/pin/create/button/?url=https%3A%2F%2Fwww.markate.com%2Fdeal%2Fdeal-2021%2F1195&amp;description=DEAL+2021&amp;" target="_blank"><span class="fa fa-pinterest"></span></a>
-    <a class="share-circle share-mail" href="mailto:?subject=DEAL%202021&amp;body=I%20thought%20you%20would%20like%20this%20Markate%20deal%3A%0A%0ADEAL%202021%0Ahttps%3A%2F%2Fwww.markate.com%2Fdeal%2Fdeal-2021%2F1195%0A%0AFor%20more%20deals%2C%20visit%20https%3A%2F%2Fwww.markate.com%2Fdeals&amp;" target="_blank"><span class="fa fa-envelope"></span></a>
-</div>
-            <br>
-
-            <div class="side-title">About PRO</div>
-            <div class="business">
-                <img class="business-img" src="https://markate.blob.core.windows.net/cdn/20200131/avatar_14356_2efeea8595_sm.jpg">
+         </div>
+         <br class="clearfix"/><br/>
+         <a class="btn btn-primary btn-lg btn-block" style="background-color:#64477d !important;" href="https://www.markate.com/deals/book/view/id/1195">Book This Deal</a>
+         <div class="text-center margin-top-sec">
+            <span class="text-ter text-sm">No credit card required</span>
+         </div>
+         <hr class="margin-top margin-bottom">
+         <div class="side-title bold">Share This Deal</div>
+         <div class="share">
+            <a class="share-circle share-facebook" style="color:#3b5998;" href="https://www.facebook.com/dialog/share?app_id=1676920742581843&amp;display=popup&amp;href=https%3A%2F%2Fwww.markate.com%2Fdeal%2Fdeal-2021%2F1195&amp;redirect_uri=https%3A%2F%2Fwww.markate.com%2Fdeal%2Fdeal-2021%2F1195&amp;" target="_blank"><span class="fa fa-facebook"></span></a>
+            <a class="share-circle share-twitter" style="color:#00acee;" href="https://twitter.com/share?url=https%3A%2F%2Fwww.markate.com%2Fdeal%2Fdeal-2021%2F1195&amp;via=Markate&amp;text=DEAL+2021&amp;" target="_blank"><span class="fa fa-twitter"></span></a>
+            <a class="share-circle share-google" style="color:#4285F4;" href="https://plus.google.com/share?url=https%3A%2F%2Fwww.markate.com%2Fdeal%2Fdeal-2021%2F1195&amp;title=DEAL+2021&amp;" target="_blank"><span class="fa fa-google"></span></a>
+            <a class="share-circle share-pinterest" style="color:#E60023;" href="http://pinterest.com/pin/create/button/?url=https%3A%2F%2Fwww.markate.com%2Fdeal%2Fdeal-2021%2F1195&amp;description=DEAL+2021&amp;" target="_blank"><span class="fa fa-pinterest"></span></a>
+            <a class="share-circle share-mail" style="color:#4285f4;" href="mailto:?subject=DEAL%202021&amp;body=I%20thought%20you%20would%20like%20this%20Markate%20deal%3A%0A%0ADEAL%202021%0Ahttps%3A%2F%2Fwww.markate.com%2Fdeal%2Fdeal-2021%2F1195%0A%0AFor%20more%20deals%2C%20visit%20https%3A%2F%2Fwww.markate.com%2Fdeals&amp;" target="_blank"><span class="fa fa-envelope"></span></a>
+         </div>
+         <br>
+         <div class="side-title bold">About PRO</div>
+         <br/>
+         <div class="business">
+            <img class="business-img" src="https://markate.blob.core.windows.net/cdn/20200131/avatar_14356_2efeea8595_sm.jpg">
+         </div>
+         <br/>
+         <ul class="business-info">
+            <li>
                 <span class="business-name">Alarm Direct</span>
                 <span class="business-location">Pensacola, FL</span>
-            </div>
-            <ul class="business-info">
-                <li>
-                    Business since 2014                </li>
-                                <li>
-                    Licensed in FL                </li>
-                                                <li>
-                    Bonded
-                </li>
-                                                <li>
-                    Insured
-                </li>
-                                            </ul>
-            <span class="text-ter">About</span>
-            <p>Panhandle’s Honeywell alarm dealership</p>
-        </div>
-    </div>
-
-
+            </li>
+            <li>
+               Business since 2014                
+            </li>
+            <li>
+               Licensed in FL                
+            </li>
+            <li>
+               Bonded
+            </li>
+            <li>
+               Insured
+            </li>
+         </ul>
+         <br/>
+         <span class="text-ter">About</span>
+         <p>Panhandle’s Honeywell alarm dealership</p>
+      </div>
+   </div>
 </div>
 <?php include viewPath('includes/footer_pages'); ?>
 
