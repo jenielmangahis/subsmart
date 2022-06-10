b3f57b8d8c1bc5f717dd1e52976e9ffda8b3c038
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2022-06-10 23:54:21 +0800

    Updated customer dashboard -> profile   Added send sms button   https://prnt.sc/lEhFrF3GhOHf   Created send sms modal form   https://prnt.sc/9HzSvwzTopS0   https://prnt.sc/k3YYdjegIWYk   Applied sms message limit text script   Added modal message if no sms api is set show modal notice   https://prnt.sc/6bROx7J5cUwh Updated api connector -> set default sms api   Added notice that cannot use ring central if there is no valid account   https://prnt.sc/DJ6VoGfgWuVT Explore twilio app Study twilio api Downloaded twilio php sdk Created test function for twilio sms Updated constants.php   Added twilio credentials

--- application/config/constants.php
+++ application/config/constants.php
@@ -163,7 +163,12 @@ define('NMI_SOFTWARE_NAME', 'NSMART');
 define('NMI_SOFTWARE_VERSION', '1.0');
 define('NMI_TEST_SERVER_URL', 'https://test.cardeasexml.com/generic.cex');
 
-//Twilio
-define('TWILIO_SID', 'ACf262b7f581f8d1eb2a3d4ddf48f0fdb2');
+//Twilio - production
+/*define('TWILIO_SID', 'ACf262b7f581f8d1eb2a3d4ddf48f0fdb2');
 define('TWILIO_TOKEN', '6b0e320e49625b6c9ce157efa14a4931');
-define('TWILIO_NUMBER', '+15017122661');
+define('TWILIO_NUMBER', '+18506195914');*/
+
+//Twilio - Test Account
+define('TWILIO_SID', 'AC73198272b9940041ef8a9d9c0b32f2c4');
+define('TWILIO_TOKEN', '86f57bbbd4a371ab738205df78113e3e');
+define('TWILIO_NUMBER', '+15005550006');
--- application/views/v2/pages/customer/adv_cust_modules/profile.php
+++ application/views/v2/pages/customer/adv_cust_modules/profile.php
@@ -1,3 +1,9 @@
+<style>
+.btn-set-customer-mobile{
+    display: block;
+    margin-top: 13px;
+}
+</style>
 <div class="col-12 col-md-4" data-id="<?= $id ?>" id="<?= $id ?>">
     <div class="nsm-card nsm-grid">
         <div class="nsm-card-header d-block">
@@ -66,7 +72,7 @@
                     <a role="button" class="nsm-button btn-sm m-0 me-2" href="<?php echo base_url("/customer/add_advance/$profile_info->prof_id"); ?>">
                         Edit Profile
                     </a>
-                </div>
+                </div>                
                 <div class="col-12 col-md-7 text-end">
                     <div class="form-check d-inline-block me-3">
                         <input class="form-check-input" type="checkbox" value="1" id="notify_by_sms" name="notify_by_sms" checked>
@@ -81,7 +87,12 @@
                         </label>
                     </div>
                 </div>
-                <div class="col-12" id="customerquickactions">
+                <div class="col-12 col-md-12 mt-5">
+                    <a role="button" class="nsm-button primary w-100 ms-0 mt-5 send-sms-message" href="javascript:void(0);" data-customer-name="<?= ucwords($profile_info->first_name) . ' ' . ucwords($profile_info->last_name) ?>" data-id="<?= $profile_info->prof_id; ?>" data-phone="<?= $profile_info->phone_m; ?>">
+                        Send SMS
+                    </a>
+                </div>
+                <div class="col-12 mb-2" id="customerquickactions">
                     <div class="d-flex justify-content-end mb-3">
                         <a href="#" class="nsm-link" id="managequickactions">Manage Quick Actions</a>
                     </div>
@@ -152,3 +163,191 @@
     </div>
   </div>
 </div>
+
+<!--Send Message Modal-->
+<div class="modal fade nsm-modal fade" id="modalSendMessage" tabindex="-1" aria-labelledby="modalSendMessageLabel" aria-hidden="true">
+    <div class="modal-dialog modal-md">
+        <div class="modal-content">
+            <div class="modal-header">
+                <span class="modal-title content-title" id="new_feed_modal_label">Send Message</span>
+                <button type="button" data-bs-dismiss="modal" aria-label="Close"><i class='bx bx-fw bx-x m-0'></i></button>
+            </div>
+            <?php if( $default_sms != ''){ ?>
+                <form action="" id="frm-send-sms-message">
+                <input type="hidden" name="cid" id="cid" value="">
+                <div class="modal-body">
+                    <div class="row">                                                                
+                        <div class="col-md-12 mt-3">
+                            <label for="">Customer</label>
+                            <input type="text" name="customer_name" id="customer-name" readonly="" disabled="" class="form-control" required="">
+                        </div>
+                        <div class="col-md-12 mt-3">
+                            <label for="">Phone Number</label>
+                            <input type="text" name="customer_phone" id="customer-phone" readonly="" disabled="" class="form-control" required="">
+                        </div>
+                        <!-- <div class="form-check grp-send-sms-notification">
+                          <input class="form-check-input" name="send_sms_notification" type="checkbox" value="" id="flexCheckDefault">
+                          <label class="form-check-label" for="flexCheckDefault">
+                            Send SMS Notification
+                          </label>
+                        </div> -->
+                        <div class="col-md-12 mt-3">
+                            <label for="">Message</label>
+                            <textarea class="form-control" name="sms_txt_message" id="sms-txt" style="height:150px;"></textarea>
+                        </div>                                
+                        <div class="help help-sm margin-bottom-sec">
+                            message characters: <span class="margin-right-sec char-counter">0</span>
+                            left characters: <span class="margin-right-sec char-counter-left">0</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="nsm-button" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="nsm-button primary btn-send-message">Send</button>
+                </div>
+                </form>
+            <?php }else{ ?>   
+                <div class="modal-body">                  
+                    <div class="alert alert-danger" style="text-align:center;" role="alert">
+                      You do not have any active SMS API account.
+                      <a class="nsm-button primary w-50 ms-0 mt-5" style="display: block;margin:15px auto !important;" href="<?= base_url('tools/api_connectors'); ?>">Setup SMS API</a>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</div>
+
+<!--Set customer mobile number Modal-->
+<div class="modal fade nsm-modal fade" id="modalSetCustomerMobile" tabindex="-1" aria-labelledby="modalSetCustomerMobileLabel" aria-hidden="true">
+    <div class="modal-dialog modal-md">
+        <div class="modal-content">
+            <div class="modal-header">
+                <span class="modal-title content-title" id="new_feed_modal_label">Set Mobile Number</span>
+                <button type="button" data-bs-dismiss="modal" aria-label="Close"><i class='bx bx-fw bx-x m-0'></i></button>
+            </div>
+            <form action="" id="frm-update-customer-mobile">
+            <input type="hidden" name="cid" id="smn-cid" value="">
+            <div class="modal-body">
+                <div class="row">                                                                
+                    <div class="col-md-12 mt-3">
+                        <label for="">Customer</label>
+                        <input type="text" name="customer_name" id="smn-customer-name" readonly="" disabled="" class="form-control" required="">
+                    </div>
+                    <div class="col-md-12 mt-3">
+                        <label for="">Phone Number</label>
+                        <input type="text" name="customer_phone" id="sms-customer-number" class="form-control" required="">
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="nsm-button" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="nsm-button primary btn-update-mobile">Save</button>
+            </div>
+            </form>                      
+        </div>
+    </div>
+</div>
+<script>
+function smsCharCounter(){
+    var chars_max   = 250;
+    var chars_total = $("#sms-txt").val().length;
+    var chars_left  = chars_max - chars_total;
+
+    $('.char-counter').html(chars_total);
+    $(".char-counter-left").html(chars_left);
+
+    return chars_left;
+}
+
+$("#sms-txt").keydown(function(e){
+    var chars_left = smsCharCounter();
+    if( chars_left <= 0 ){
+        if (e.keyCode != 46 && e.keyCode != 8 ) return false;
+    }else{
+        return true;
+    }
+});
+<?php if( $default_sms != ''){ ?>
+smsCharCounter();
+<?php } ?>
+
+$(document).on('click', '.send-sms-message', function(){
+    var customer_name = $(this).attr('data-customer-name');
+    var profid = $(this).attr('data-id');
+    var customer_phone = $(this).attr('data-phone');
+
+    if( customer_phone != '' ){
+        $('#cid').val(profid);
+        $('#customer-name').val(customer_name);
+        $('#customer-phone').val(customer_phone);
+        $('#modalSendMessage').modal('show');
+        $('#modalMessagesSent').modal('hide');
+    }else{
+        var msg = 'Phone number is needed to send sms. <br /><a href="javascript:void(0);" data-customer-name="'+customer_name+'" data-id="'+profid+'" class="nsm-button primary btn-set-customer-mobile">Set Mobile Number</a>'
+        Swal.fire({
+            icon: 'error',
+            title: 'Error!',
+            html: msg,
+            showConfirmButton: false
+        });
+    }        
+});
+
+$(document).on('click', '.btn-set-customer-mobile', function(){
+    var customer_name = $(this).attr('data-customer-name');
+    var cid = $(this).attr('data-id');
+
+    swal.close()
+
+    $('#smn-customer-name').val(customer_name);
+    $('#smn-cid').val(cid);
+    $('#modalSendMessage').modal('hide');
+    $('#modalSetCustomerMobile').modal('show');
+});
+
+$(document).on('submit', '#frm-send-sms-message', function(e){
+    e.preventDefault();
+    var url = base_url + 'messages/_company_send';
+    $(".btn-send-message").html('<span class="bx bx-loader bx-spin"></span>');
+
+    var formData = new FormData($("#frm-send-sms-message")[0]);   
+
+    setTimeout(function () {
+      $.ajax({
+         type: "POST",
+         url: url,
+         dataType: 'json',
+         contentType: false,
+         cache: false,
+         processData:false,
+         data: formData,
+         success: function(o)
+         {          
+            if( o.is_success == 1 ){   
+                $("#modalSendMessage").modal("hide");         
+                Swal.fire({
+                    title: 'Save Successful!',
+                    text: "Your message was successfully sent to customer.",
+                    icon: 'success',
+                    showCancelButton: false,
+                    confirmButtonText: 'Okay'
+                }).then((result) => {
+                    //if (result.value) {
+                    location.reload();
+                    //}
+                });
+            }else{
+              Swal.fire({
+                icon: 'error',
+                title: 'Error!',
+                html: o.msg
+              });
+            } 
+
+            $(".btn-send-message").html('Save');
+         }
+      });
+    }, 800);
+});
+</script>
