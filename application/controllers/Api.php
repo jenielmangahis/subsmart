f642efded7a0f1ef26a9f2f537e4a7ce459fc277
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2022-08-17 16:56:02 +0800

    test api auto sms

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -355,7 +355,9 @@ class Api extends MYF_Controller
 
     public function create_auto_sms_notification()
     {
-        $data       = $this->input->post();
+        $data  = $this->input->post();
+        echo "<pre>";
+        print_r($post);
         $is_success = createCronAutoSmsNotification($data['company_id'], $data['object_id'], $data['module_name'], $data['status'], $data['user_id'], $data['agent_id']);
 
         $result = ['is_success' => $is_success];
