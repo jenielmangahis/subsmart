9fb83129ab1386774319768b47b58a050bf54d61
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2022-08-17 17:11:52 +0800

    Removed print_r

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -356,13 +356,11 @@ class Api extends MYF_Controller
     public function create_auto_sms_notification()
     {
         $data  = $this->input->post();
-        echo "<pre>";
-        print_r($data);
         $is_success = createCronAutoSmsNotification($data['company_id'], $data['object_id'], $data['module_name'], $data['status'], $data['user_id'], $data['assigned_user_id'], $data['agent_id']);
 
         $result = ['is_success' => $is_success];
-
         echo json_encode($result);
+        
         exit;   
     }
 }
