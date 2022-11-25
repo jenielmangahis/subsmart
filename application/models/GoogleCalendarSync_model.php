66316dda60f420e3e3eb21f99143f15558e8f91f
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2022-11-25 22:00:03 +0800

    Added missing file

---
+++ application/models/GoogleCalendarSync_model.php
@@ -0,0 +1,57 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class GoogleCalendarSync_model extends MY_Model
+{
+    public $table = 'google_calendar_sync';
+
+    public function getAll($filters=array())
+    {
+        $id = logged('id');
+
+        $this->db->select('*');
+        $this->db->from($this->table);
+
+        if ( !empty($filters) ) {
+            if ( !empty($filters['search']) ) {
+                $this->db->like('title', $filters['search'], 'both');
+            }
+        }
+
+        $this->db->where('user_id', $id);
+        $this->db->order_by('id', 'DESC');
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getById($id)
+    {
+        $user_id = logged('id');
+
+        $this->db->select('*');
+        $this->db->from($this->table);
+
+        $this->db->where('user_id', $user_id);
+        $this->db->where('id', $id);
+
+        $query = $this->db->get()->row();
+        return $query;
+    }
+
+    public function getAllToSync()
+    {
+        $id = logged('id');
+
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('is_sync', 0);
+        $this->db->order_by('id', 'ASC');
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+}
+
+/* End of file BookingCoupon_model.php */
+/* Location: ./application/models/BookingCoupon_model.php */
