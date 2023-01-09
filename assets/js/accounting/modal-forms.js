4ae63241aab3da6bcbdff4ddb16f08df25edf554
Bryann Revina <bryann.revina03@gmail.com>
2023-01-09 16:45:18 +0800

    Update files

--- assets/js/accounting/modal-forms.js
+++ assets/js/accounting/modal-forms.js
@@ -2641,7 +2641,7 @@ $(function() {
                         `);
 
                         $('#expenseModal .modal-body').children('.row').append(`
-                            <div class="w-auto nsm-callout primary" style="max-width: 15%">
+                            <div class="nsm-callout primary" style="width: 15%">
                                 <div class="transactions-container h-100 p-3">
                                     <div class="row">
                                         <div class="col-12">
@@ -2720,7 +2720,7 @@ $(function() {
                         `);
 
                         $('#checkModal .modal-body').children('.row').append(`
-                            <div class="w-auto nsm-callout primary" style="max-width: 15%">
+                            <div class="nsm-callout primary" style="width: 15%">
                                 <div class="transactions-container h-100 p-3">
                                     <div class="row">
                                         <div class="col-12">
@@ -2797,7 +2797,7 @@ $(function() {
                     `);
 
                     $('#billModal .modal-body').children('.row').append(`
-                        <div class="w-auto nsm-callout primary" style="max-width: 15%">
+                        <div class="nsm-callout primary" style="width: 15%">
                             <div class="transactions-container h-100 p-3">
                                 <div class="row">
                                     <div class="col-12">
@@ -3149,6 +3149,12 @@ $(function() {
                     }
 
                     button.parent().append(`<input type="hidden" value="${data.type.replace('-', '_')+'-'+details.id}" name="linked_transaction[]">`);
+
+                    if ($('#modal-container .modal .transactions-container .row div.col-12').length === 1) {
+                        $('#modal-container .modal .transactions-container').parent().remove();
+                        $('#modal-container .modal .close-transactions-container').remove();
+                        $('#modal-container .modal .open-transactions-container').remove();
+                    }
                 });
 
                 $(this).closest('.card').parent().remove();
@@ -3160,12 +3166,6 @@ $(function() {
                 $('#modal-container form .modal .transactions-container .row div.col-12 .add-transaction[data-type="vendor-credit"]').each(function() {
                     $(this).closest('.card').parent().remove();
                 });
-
-                // if ($('#modal-container .modal .transactions-container .row div.col-12').length === 1) {
-                //     $('#modal-container .modal .transactions-container').parent().remove();
-                //     $('#modal-container .modal .close-transactions-container').remove();
-                //     $('#modal-container .modal .open-transactions-container').remove();
-                // }
             break;
             case 'bill':
                 if ($('#modal-container form .modal').attr('id') !== 'billPaymentModal') {
@@ -3480,6 +3480,12 @@ $(function() {
                             </div>
                         `);
                     }
+
+                    if ($('#modal-container .modal .transactions-container .row div.col-12').length === 2) {
+                        $('#modal-container .modal .transactions-container').parent().remove();
+                        $('#modal-container .modal .close-transactions-container').remove();
+                        $('#modal-container .modal .open-transactions-container').remove();
+                    }
                 });
 
                 $(this).parent().parent().parent().parent().parent().remove();
@@ -3683,6 +3689,12 @@ $(function() {
                                 </div>
                             </div>
                         `);
+                    }
+
+                    if ($('#modal-container .modal .transactions-container .row div.col-12').length === 2) {
+                        $('#modal-container .modal .transactions-container').parent().remove();
+                        $('#modal-container .modal .close-transactions-container').remove();
+                        $('#modal-container .modal .open-transactions-container').remove();
                     }
                 });
 
@@ -7379,7 +7391,7 @@ $(function() {
                             $('#invoiceModal .transactions-container').parent().remove();
 
                             $('#invoiceModal .modal-body').children('.row').append(`
-                                <div class="w-auto nsm-callout primary" style="max-width: 15%">
+                                <div class="nsm-callout primary" style="width: 15%">
                                     <div class="transactions-container h-100 p-3">
                                         <div class="row">
                                             <div class="col-12">
@@ -12919,7 +12931,7 @@ const get_bill_payment_linkable_transactions = () => {
             `);
 
             $('#billPaymentModal .modal-body').children('.row').append(`
-                <div class="w-auto nsm-callout primary" style="max-width: 15%">
+                <div class="nsm-callout primary" style="width: 15%">
                     <div class="transactions-container h-100 p-3">
                         <div class="row">
                             <div class="col-12">
