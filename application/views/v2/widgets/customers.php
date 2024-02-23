296ed16b5165bffb49ff6217b1c7538101022c0d
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-02-23 15:34:50 +0800

    Added customer widget

---
+++ application/views/v2/widgets/customers.php
@@ -0,0 +1,41 @@
+<?php
+if (!is_null($dynamic_load) && $dynamic_load == true) :
+    echo '<div class="col-12 col-lg-4">';
+endif;
+?>
+
+<div class="<?= $class ?>" data-id="<?= $id ?>" id="widget_<?= $id ?>" draggable="true">
+    <div class="nsm-card-header">
+        <div class="nsm-card-title">
+            <span>Recent Customers</span>
+        </div>
+        <div class="nsm-card-controls">
+            <a role="button" class="nsm-button btn-sm m-0 me-2" href="<?= base_url() ?><?php if($var==1){echo "customer";}else{echo "job";} ?>">
+                See More
+            </a>
+            <div class="dropdown">
+                <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown">
+                    <i class='bx bx-fw bx-dots-vertical-rounded'></i>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <li><a class="dropdown-item" href="#" onclick="addToMain('<?= $id ?>',<?php echo ($isMain ? '1' : '0') ?>,'<?= $isGlobal ?>' )"><?php echo ($isMain ? 'Remove From Main' : 'Add to Main') ?></a></li>
+                    <li><a class="dropdown-item" href="#" onclick="removeWidget('<?= $id ?>');">Remove Widget</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="nsm-card-content">
+        <div class="nsm-widget-table">
+            <div id="jobsData">
+            </div>
+            <div id="companiesData">
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+if (!is_null($dynamic_load) && $dynamic_load == true) :
+    echo '</div>';
+endif;
+?>
