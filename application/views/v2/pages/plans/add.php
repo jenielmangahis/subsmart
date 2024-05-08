717a810e02cdd79e1c979b20058a559701eeabda
Bryann Revina <bryann.revina03@gmail.com>
2024-05-08 21:48:47 +0800

    updated nav

--- application/views/v2/pages/plans/add.php
+++ application/views/v2/pages/plans/add.php
@@ -141,7 +141,7 @@ label>input {
 
 <div class="row page-content g-0">
     <div class="col-12 mb-3">
-        <?php include viewPath('v2/includes/page_navigations/sales_tabs'); ?>
+        <?php include viewPath('v2/includes/page_navigations/estimate__tabs_v2'); ?>
     </div>
     <div class="col-12 mb-3">
         <?php include viewPath('v2/includes/page_navigations/estimate_subtabs'); ?>
