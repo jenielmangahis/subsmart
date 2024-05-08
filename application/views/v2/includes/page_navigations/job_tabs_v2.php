7ed4da6a14d36e06173b2147d506c3948f8a8410
Bryann Revina <bryann.revina03@gmail.com>
2024-05-08 19:23:24 +0800

    Updated navs

--- application/views/v2/includes/page_navigations/job_tabs_v2.php
+++ application/views/v2/includes/page_navigations/job_tabs_v2.php
@@ -1,6 +1,12 @@
 <div class="nsm-page-nav">
     <ul>
-        <li class="<?php if($page->title == 'Jobs' || $page->title == 'Job Types' || $page->title == 'Job Tags' || $page->title == 'Bird\'s Eye View' || $page->title == 'Checklist' || $page->title == 'Job Settings'): echo 'active'; endif; ?>">
+        <li class="active">
+            <a class="nsm-page-link" href="<?= base_url('job') ?>">
+                <i class='bx bx-fw bx-message-square-error'></i>
+                <span>Jobs</span>
+            </a>
+        </li>    
+        <!-- <li class="<?php if($page->title == 'Jobs' || $page->title == 'Job Types' || $page->title == 'Job Tags' || $page->title == 'Bird\'s Eye View' || $page->title == 'Checklist' || $page->title == 'Job Settings'): echo 'active'; endif; ?>">
             <a class="nsm-page-link" href="<?= base_url('job') ?>">
                 <i class='bx bx-fw bx-message-square-error'></i>
                 <span>Jobs</span>
@@ -11,7 +17,7 @@
                 <i class='bx bx-fw bx-note'></i>
                 <span>Service Tickets</span>
             </a>
-        </li>
+        </li> -->
         <!-- Do not remove the last li -->
         <li><label></label></li>
     </ul>
