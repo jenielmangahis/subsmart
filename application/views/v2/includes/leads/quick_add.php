dc0f534b7828b9bd0939556ffdf758f564aa2ba3
Bryann Revina <bryann.revina03@gmail.com>
2024-05-20 20:50:53 +0800

    Fix menu items Added accounting items

--- application/views/v2/includes/leads/quick_add.php
+++ application/views/v2/includes/leads/quick_add.php
@@ -42,13 +42,13 @@
                         <div class="col-md-6 mb-2">
                             <div class="form-group">
                                 <label>Mobile</label>
-                                <input type="text" name="phone_cell" class="form-control" required>
+                                <input type="text" name="phone_cell" class="form-control lead_phone_number" maxlength="12" placeholder="xxx-xxx-xxxx" required>
                             </div>
                         </div>
                         <div class="col-md-6 mb-2">
                             <div class="form-group">
                                 <label>Phone</label>
-                                <input type="text" name="phone_home" class="form-control">
+                                <input type="text" name="phone_home" class="form-control lead_phone_number" maxlength="12" placeholder="xxx-xxx-xxxx">
                             </div>
                         </div>
                         <div class="col-md-12 mb-2">
@@ -122,6 +122,24 @@ $(function(){
                 }
             }
         });
+    });
+
+    $('#frm-quick-add-lead').on('shown.bs.modal', function (e) {
+        $('#new_customer_form')[0].reset();
+    });
+
+    $('.lead_phone_number').keydown(function(e) {
+        var key = e.charCode || e.keyCode || 0;
+        $text = $(this);
+        if (key !== 8 && key !== 9) {
+            if ($text.val().length === 3) {
+                $text.val($text.val() + '-');
+            }
+            if ($text.val().length === 7) {
+                $text.val($text.val() + '-');
+            }
+        }
+        return (key == 8 || key == 9 || key == 46 || (key >= 48 && key <= 57) || (key >= 96 && key <= 105));
     });
 });
 </script>
