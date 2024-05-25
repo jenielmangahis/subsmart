bdac06d32a127747fbd93b03db6aabbd7b54f891
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-05-25 11:43:16 +0800

    widget fixes

--- application/views/v2/widgets/jobs.php
+++ application/views/v2/widgets/jobs.php
@@ -24,7 +24,7 @@
     <div class="nsm-card-content">
         <div class="row mb-4 mt-2">
             <div class="col-4">
-                <select class="nsm-field form-select" name="filter_date" id="widget-service-ticket-filter-date">   
+                <select class="nsm-field form-select" name="filter_date" id="widget-job-filter-date">   
                     <option value="custom">Custom</option>
                     <option value="this-month">This month</option>
                     <option value="this-quarter">This quarter</option>
@@ -32,16 +32,202 @@
                 </select>
             </div>
             <div class="col-4">
-                <input type="text" id="widget-service-ticket-filter-from" class="nsm-field form-control widget-service-ticket-datepicker" value="<?= date("01/Y"); ?>" />
+                <input type="text" id="widget-job-filter-from" class="nsm-field form-control widget-job-datepicker" value="<?= date("01/Y"); ?>" />
             </div>
             <div class="col-4">
-                <input type="text" id="widget-service-ticket-filter-to" class="nsm-field form-control widget-service-ticket-datepicker" value="<?= date("12/Y"); ?>" required>
+                <input type="text" id="widget-job-filter-to" class="nsm-field form-control widget-job-datepicker" value="<?= date("12/Y"); ?>" required>
             </div>
         </div>  
         <canvas id="jobs_chart" class="nsm-chart" data-chart-type="jobs"></canvas>
     </div>
 </div>
+<script>
+$(function(){
+    var jobs = $('#jobs_chart');
+    var jobsChart = new Chart(jobs, {
+        type: 'line',        
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'bottom',
+                },
+            },
+            aspectRatio: 1,
+            scales: {
+                A: {
+                    type: 'linear',
+                    position: 'left',
+                    title: {
+                        display: true,
+                        text: 'No of Jobs'
+                    },
+                    ticks: {
+                        beginAtZero: true,
+                        callback: function(value, index, values) {
+                            if (parseInt(value) >= 1000) {
+                                return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g,
+                                    ",");
+                            } else {
+                                return value;
+                            }
+                        }
+                    }
+                },
+                B: {
+                    type: 'linear',
+                    position: 'right',
+                    ticks: {
+                        beginAtZero: true,
+                        callback: function(value, index, values) {
+                            if (parseInt(value) >= 1000) {
+                                return '$' + value.toString().replace(
+                                    /\B(?=(\d{3})+(?!\d))/g, ",");
+                            } else {
+                                return '$' + value;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
 
+    $(".widget-job-datepicker").datepicker( {
+        format: "mm/yyyy",
+        viewMode: "months", 
+        minViewMode: "months"
+    });
+
+    $('#widget-job-filter-date').on('change', function(){
+        switch($(this).val()) {            
+            case 'this-month' :
+                var date = new Date();
+                var to_date = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+
+                from_date = String(date.getMonth() + 1).padStart(2, '0') + '/' + String(1).padStart(2, '0') + '/' + date.getFullYear();
+                to_date = String(to_date.getMonth() + 1).padStart(2, '0') + '/' + String(to_date.getDate()).padStart(2, '0') + '/' + to_date.getFullYear();
+            break;
+            case 'this-quarter' :
+                var date = new Date();
+                var currQuarter = Math.floor(date.getMonth() / 3 + 1);
+                
+                switch(currQuarter) {
+                    case 1 :
+                        var from_date = '01/01/' + date.getFullYear();
+                        var to_date = '03/31/'+ date.getFullYear();
+                    break;
+                    case 2 :
+                        var from_date = '04/01/' + date.getFullYear();
+                        var to_date = '06/30/'+ date.getFullYear();
+                    break;
+                    case 3 :
+                        var from_date = '07/01/' + date.getFullYear();
+                        var to_date = '09/30/'+ date.getFullYear();
+                    break;
+                    case 4 :
+                        var from_date = '10/01/' + date.getFullYear();
+                        var to_date = '12/31/'+ date.getFullYear();
+                    break;
+                }
+            break;
+            case 'this-year' :
+                var date = new Date();
+
+                var from_date = String(1).padStart(2, '0') + '/' + String(1).padStart(2, '0') + '/' + date.getFullYear();
+                var to_date = String(12).padStart(2, '0') + '/' + String(31).padStart(2, '0') + '/' + date.getFullYear();
+            break;
+            default :
+                var date = new Date();
+                var to_date = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+
+                from_date = String(date.getMonth() + 1).padStart(2, '0') + '/' + String(1).padStart(2, '0') + '/' + date.getFullYear();
+                to_date = String(to_date.getMonth() + 1).padStart(2, '0') + '/' + String(to_date.getDate()).padStart(2, '0') + '/' + to_date.getFullYear();
+            break;
+        }
+
+        $('#widget-job-filter-from').val(moment(from_date).format('MM/YYYY'));
+        $('#widget-job-filter-to').val(moment(to_date).format('MM/YYYY'));
+        
+        loadJobChart();
+    });
+
+    $('#widget-job-filter-from, #widget-job-filter-to').on('change', function(){
+        loadJobChart();
+    });
+
+    loadJobChart();
+
+    function loadJobChart(){
+        var filter_date_from = $('#widget-job-filter-from').val();
+        var filter_date_to   = $('#widget-job-filter-to').val();
+
+        $.ajax({
+            url: base_url + 'widgets/_load_job_chart_data',
+            method: 'post',
+            data: {filter_date_from:filter_date_from,filter_date_to:filter_date_to},
+            dataType: 'json',
+            success: function (data) {              
+                const jobs_labels = data.chart_labels;
+                const jobs_data = {
+                    labels: jobs_labels,
+                    datasets: [{
+                        label: 'Job Count',                        
+                        data: data.total_jobs_number_data,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1,
+                        stack: 'combined',
+                        type: 'bar',
+                        yAxisID: "A",
+                    },
+                    {
+                        label: 'Job Value',
+                        data: data.total_jobs_amount_data,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(191, 191, 191, 0.5)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1,
+                        stack: 'combined',
+                        yAxisID: "B",
+                    }
+                    ]
+                };
+
+                jobsChart.data = jobs_data;
+                jobsChart.update();
+            }
+        });
+    }
+});
+</script>
 
 <?php
     if(!is_null($dynamic_load) && $dynamic_load == true):
