5d509d281e8a33913a3e3671ba416590b462635d
Bryann Revina <bryann.revina03@gmail.com>
2024-05-25 15:26:01 +0800

    Updated method

--- application/models/Accounting_expense.php
+++ application/models/Accounting_expense.php
@@ -4,5 +4,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Accounting_expense extends MY_Model {
 
     public $table = 'accounting_expense';    
+
+    public function getAll($limit = 0)
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('is_updated', 0);
+
+        if( $limit > 0 ){
+            $this->db->limit($limit);
+        } 
+
+        $this->db->order_by('id', 'DESC');
+        
+        $query = $this->db->get();
+        return $query->result();
+    }
 	
 }
