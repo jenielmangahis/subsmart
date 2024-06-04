7546e588957b8b9b0337d87b7b37ecd66c5d3006
Bryann Revina <bryann.revina03@gmail.com>
2024-06-04 11:35:36 +0800

    Added subnavs

---
+++ application/views/v2/includes/page_navigations/birds_eye_view_subtabs.php
@@ -0,0 +1,9 @@
+<div class="nsm-page-subnav">
+    <ul>
+        <li class="<?php if($page->title == 'Bird\'s Eye View'): echo 'active'; endif; ?>" onclick="location.href='<?= base_url('job/bird_eye_view') ?>'">
+            <a class="nsm-page-link" href="javascript:void(0);">
+                <span>Bird's Eye View</span>
+            </a>
+        </li>
+    </ul>
+</div>
---
+++ application/views/v2/includes/page_navigations/birds_eye_view_tabs.php
@@ -0,0 +1,12 @@
+<div class="nsm-page-nav">
+    <ul>
+        <li class="active">
+            <a class="nsm-page-link" href="<?= base_url('job') ?>">
+                <i class='bx bx-fw bx-message-square-error'></i>
+                <span>Bird's Eye View</span>
+            </a>
+        </li>   
+        <!-- Do not remove the last li -->
+        <li><label></label></li>
+    </ul>
+</div>
