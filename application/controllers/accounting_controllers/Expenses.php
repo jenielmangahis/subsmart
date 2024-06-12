eb34b496cad34c59def3567730065ee0d0e0f881
Bryann Revina <bryann.revina03@gmail.com>
2024-06-12 10:30:34 +0800

    merged changes widget changes

--- application/controllers/accounting_controllers/Expenses.php
+++ application/controllers/accounting_controllers/Expenses.php
@@ -245,7 +245,7 @@ class Expenses extends MY_Controller
                             <ul class="dropdown-menu dropdown-menu-end">';
                         
                         if($bill->status !== "2") {
-                            $manageCol .= '<li><a class="dropdown-item" href="#">Schedule payment</a></li>';
+                            $manageCol .= '<li><a class="dropdown-item schedule-payment" href="#">Schedule payment</a></li>';
                             $manageCol .= '<li><a class="dropdown-item mark-as-paid-transaction" href="#">Mark as paid</a></li>';
                         }
                         $manageCol .= '<li>
