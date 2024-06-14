45e3784ebc4f0f4a85d1a2da2f49bb24d61209ed
Bryann Revina <bryann.revina03@gmail.com>
2024-06-14 15:40:26 +0800

    Updated routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -943,6 +943,9 @@ $route['settings/_edit_sms_template'] = 'Settings/ajax_edit_sms_template';
 $route['settings/_add_tax_rate'] = 'Settings/ajax_add_tax_rate';
 $route['settings/_update_tax_rate'] = 'Settings/ajax_update_tax_rate';
 $route['settings/_delete_tax_rate'] = 'Settings/ajax_delete_tax_rate';
+$route['settings/_update_calendar_settings'] = 'Settings/ajax_update_calendar_settings';
+
+//Chatbot
 $route['chatbot/settings'] = 'ChatbotSettings/settings';
 
 // leads
