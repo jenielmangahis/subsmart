a8fd4a58f101167e2d61682b86e6555d82ce97f2
Bryann Revina <bryann.revina03@gmail.com>
2024-07-10 22:08:11 +0800

    Updates (Jeniel - July 10) -Merge updates

--- application/models/Accounting_model.php
+++ application/models/Accounting_model.php
@@ -784,7 +784,7 @@ class Accounting_model extends MY_Model
             return $data->result();
         }
 
-        if ($reportType == "profit_and_loss_percentage_income") {
+        if ($reportType == "profit_and_loss_percentage_income_backup") {
             $this->db->select('
                 invoices.customer_id AS customer_id, 
                 CONCAT(acs_profile.first_name, " ", acs_profile.last_name) AS customer, 
@@ -801,6 +801,26 @@ class Accounting_model extends MY_Model
             $data = $this->db->get();
             return $data->result();
         }          
+
+        if ($reportType == "profit_and_loss_percentage_income") {
+            $this->db->select('
+                invoices.customer_id AS customer_id, 
+                CONCAT(acs_profile.first_name, " ", acs_profile.last_name) AS customer, 
+                invoices.date_issued, 
+                invoices.grand_total AS amount
+            ');
+            $this->db->from('invoices');
+            $this->db->join('acs_profile', 'acs_profile.prof_id = invoices.customer_id', 'left');
+            $this->db->where('invoices.company_id', $companyID);
+            $this->db->where('acs_profile.first_name !=', '');
+            $this->db->where("DATE_FORMAT(invoices.date_issued,'%Y-%m-%d') >= '$reportConfig[date_from]'");
+            $this->db->where("DATE_FORMAT(invoices.date_issued,'%Y-%m-%d') <= '$reportConfig[date_to]'");            
+            //$this->db->order_by($reportConfig['sort_by'], $reportConfig['sort_order']);
+            $this->db->group_by('invoices.customer_id');
+            $this->db->limit($reportConfig['page_size']);
+            $data = $this->db->get();
+            return $data->result();
+        } 
 
         // Get Balance Sheet Data in Database
         if ($reportType == "balance_sheet") {
