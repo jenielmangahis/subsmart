94a1797e6d5eaaf107e294d0ef52da48cb26b069
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-08-10 11:14:26 +0800

    Merged changes

--- application/models/Accounting_user_employment_details_model.php
+++ application/models/Accounting_user_employment_details_model.php
@@ -12,15 +12,21 @@ class Accounting_user_employment_details_model extends MY_Model {
 
     public function get_employment_details_by_worksite($worksiteId)
     {
+        
         $this->db->where('work_location_id', $worksiteId);
+
         $query = $this->db->get($this->table);
         return $query->result();
     }
 
     public function get_employment_details($userId)
     {
-        $this->db->where('user_id', $userId);
-        $query = $this->db->get($this->table);
+        $this->db->select('accounting_user_employment_details.*,accounting_worksites.name AS worksite_name, accounting_worksites.street AS worksite_address, accounting_worksites.city AS worksite_city, accounting_worksites.state AS worksite_state, accounting_worksites.zip_code AS worksite_zipcode');
+        $this->db->from($this->table);
+        $this->db->join('accounting_worksites', 'accounting_user_employment_details.work_location_id = accounting_worksites.id', 'left');
+        $this->db->where('accounting_user_employment_details.user_id', $userId);
+
+        $query = $this->db->get();
         return $query->row();
     }
 
@@ -29,5 +35,16 @@ class Accounting_user_employment_details_model extends MY_Model {
         $this->db->where('user_id', $userId);
         $update = $this->db->update($this->table, $data);
         return $update;
+    }
+
+    public function get_all_employment_details($userId)
+    {
+        $this->db->select('accounting_user_employment_details.*,accounting_worksites.name AS worksite_name, accounting_worksites.street AS worksite_address, accounting_worksites.city AS worksite_city, accounting_worksites.state AS worksite_state, accounting_worksites.zip_code AS worksite_zipcode');
+        $this->db->from($this->table);
+        $this->db->join('accounting_worksites', 'accounting_user_employment_details.work_location_id = accounting_worksites.id', 'left');
+        $this->db->where('accounting_user_employment_details.user_id', $userId);
+
+        $query = $this->db->get();
+        return $query->result();
     }
 }
