2de8afeb835be7a8bf9b49dea6c7e65b459c7398
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-08-27 17:17:57 +0800

    Merged changes

--- application/views/accounting/reports/management_reports.php
+++ application/views/accounting/reports/management_reports.php
@@ -1,5 +1,9 @@
 <?php include viewPath('v2/includes/accounting_header'); ?>
-
+<style>
+.accordion-body{
+    min-height:224px;
+}
+</style>
 <div class="row page-content g-0">
     <div class="col-12 mb-3">
         <?php include viewPath('v2/includes/page_navigations/accounting/subtabs/reports_subtabs'); ?>
@@ -11,94 +15,170 @@
                     <div class="col-12">
                         <div class="nsm-callout primary">
                             <button><i class='bx bx-x'></i></button>
-                            CRM reporting helps businesses in a few key ways: It can helps you distill what is happening in your business, a key advantage of deploying a CRM. Your data will help provides different ways to make strategic business decisions. Your management team can track performance and make tactical changes where necessary.
+                            CRM reporting helps businesses in a few key ways: It can helps you distill what is happening
+                            in your business, a key advantage of deploying a CRM. Your data will help provides different
+                            ways to make strategic business decisions. Your management team can track performance and
+                            make tactical changes where necessary.
                         </div>
                     </div>
                 </div>
+                <div class="row g-3 grid-mb favorites-item-container">
+                    <div class="col-12">
+                        <div class="accordion">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button content-title" type="button"
+                                        data-bs-toggle="collapse"
+                                        data-bs-target="#collapse-management" aria-expanded="true"
+                                        aria-controls="collapse-management">
+                                        Reports
+                                    </button>
+                                </h2>
+                                <div id="collapse-management" class="accordion-collapse collapse show">
+                                    <div class="accordion-body">
+                                        <div class="row">    
+                                            <?php foreach ($managementReports as $reportType) { ?>                                              
+                                            <div class="col-12 col-md-3">
+                                                <ul class="list-unstyled m-0">                      
+                                                    <li class="p-3 cursor-pointer">
+                                                        <span
+                                                            onclick="location.href='<?php echo is_null($reportType->url) ? base_url('/accounting/reports/view-report/'.$reportType->id) : base_url($reportType->url); ?>'"><?php echo $reportType->name; ?></span>
+                                                        <a href="#" style="color: #888888" data-bs-toggle="collapse"
+                                                            data-bs-target="#<?php echo str_replace(' ', '-', strtolower($reportGroup->description)); ?>-<?php echo $reportType->id; ?>-collapse"
+                                                            aria-expanded="false"
+                                                            aria-controls="<?php echo str_replace(' ', '-', strtolower($reportGroup->description)); ?>-<?php echo $reportType->id; ?>-collapse">
+                                                            <i class="bx bx-fw bx-help-circle report-help-popover" data-toggle="popover" data-bs-content="<?= $reportType->description; ?>"></i>
+                                                        </a>
 
-                <table class="nsm-table">
-                    <thead>
-                        <tr>
-                            <td data-name="Name">NAME</td>
-                            <td data-name="Created By">CREATED BY</td>
-                            <td data-name="Last Modified">LAST MODIFIED</td>
-                            <td data-name="Report Period">REPORT PERIOD</td>
-                            <td data-name="Manage"></td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Test Management Report</td>
-                            <td>John Doe</td>
-                            <td></td>
-                            <td>
-                                <select name="report_period[]" class="nsm-field form-select">
-                                    <option value="all-date">All Dates</option>
-                                    <option value="custom">Custom</option>
-                                    <option value="today">Today</option>
-                                    <option value="this-week">This Week</option>
-                                    <option value="this-week-to-date">This Week-to-date</option>
-                                    <option value="this-month">This Month</option>
-                                    <option value="this-month-to-date">This Month-to-date</option>
-                                    <option value="this-quarter">This Quarter</option>
-                                    <option value="this-quarter-to-date">This Quarter-to-date</option>
-                                    <option value="this-year" selected>This Year</option>
-                                    <option value="this-year-to-date">This Year-to-date</option>
-                                    <option value="this-year-to-last-month">This Year-to-last-month</option>
-                                    <option value="yesterday">Yesterday</option>
-                                    <option value="recent">Recent</option>
-                                    <option value="last-week">Last Week</option>
-                                    <option value="last-week-to-date">Last Week-to-date</option>
-                                    <option value="last-month">Last Month</option>
-                                    <option value="last-month-to-date">Last Month-to-date</option>
-                                    <option value="last-quarter">Last Quarter</option>
-                                    <option value="last-quarter-to-date">Last Quarter-to-date</option>
-                                    <option value="last-year">Last Year</option>
-                                    <option value="last-year-to-date">Last Year-to-date</option>
-                                    <option value="since-30-days-ago">Since 30 Days Ago</option>
-                                    <option value="since-60-days-ago">Since 60 Days Ago</option>
-                                    <option value="since-90-days-ago">Since 90 Days Ago</option>
-                                    <option value="since-365-days-ago">Since 365 Days Ago</option>
-                                    <option value="next-week">Next Week</option>
-                                    <option value="next-4-weeks">Next 4 Weeks</option>
-                                    <option value="next-month">Next Month</option>
-                                    <option value="next-quarter">Next Quarter</option>
-                                    <option value="next-year">Next Year</option>
-                                </select>
-                            </td>
-                            <td>
-                                <div class="dropdown table-management">
-                                    <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown">
-                                        <i class='bx bx-fw bx-dots-vertical-rounded'></i>
-                                    </a>
-                                    <ul class="dropdown-menu dropdown-menu-end">
-                                        <li>
-                                            <a class="dropdown-item" href="#">View</a>
-                                        </li>
-                                        <li>
-                                            <a class="dropdown-item" href="#">Edit</a>
-                                        </li>
-                                        <li>
-                                            <a class="dropdown-item" href="#">Send</a>
-                                        </li>
-                                        <li>
-                                            <a class="dropdown-item" href="#">Export as PDF</a>
-                                        </li>
-                                        <li>
-                                            <a class="dropdown-item" href="#">Export as DOCX</a>
-                                        </li>
-                                        <li>
-                                            <a class="dropdown-item" href="#">Copy</a>
-                                        </li>
-                                    </ul>
+                                                        <div class="dropdown float-end d-inline-block"
+                                                            style="min-width: 23px; min-height: 1px">                                                            
+                                                            <a href="#" class="dropdown-toggle"
+                                                                data-bs-toggle="dropdown" style="color: #888888">
+                                                                <i class='bx bx-fw bx-dots-vertical-rounded'></i>
+                                                            </a>
+                                                            <ul class="dropdown-menu dropdown-menu-end">                                                                
+                                                                <li><a class="dropdown-item btn-remove-management-reports" href="javascript:void(0);" data-name="<?= $reportType->name; ?>" data-id="<?= $reportType->id; ?>">Remove</a></li>
+                                                            </ul>                                                            
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            <?php } ?>
+                                        </div>
+                                    </div>
                                 </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                            </div>
+                        </div>                    
+                    </div>
+
+
+                <?php foreach ($reportGroups as $reportGroup) { ?>                
+                    <div class="col-6">
+                        <div class="accordion">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button content-title" type="button"
+                                        data-bs-toggle="collapse"
+                                        data-bs-target="#collapse-<?php echo $reportGroup->id; ?>" aria-expanded="true"
+                                        aria-controls="collapse-<?php echo $reportGroup->id; ?>">
+                                        <?php echo $reportGroup->description; ?>
+                                    </button>
+                                </h2>
+                                <div id="collapse-<?php echo $reportGroup->id; ?>"
+                                    class="accordion-collapse collapse show">
+                                    <div class="accordion-body">
+                                        <div class="row">
+                                            <?php $reportTypesColumns = count($reportGroup->report_types) > 4 ? array_chunk($reportGroup->report_types, ceil(count($reportGroup->report_types) / 2)) : [$reportGroup->report_types]; ?>
+                                            <?php foreach ($reportTypesColumns as $colRepTypes) { ?>
+                                            <div class="col-12 col-md-6">
+                                                <ul class="list-unstyled m-0">
+                                                    <?php foreach ($colRepTypes as $reportType) { ?>
+                                                    <?php $favorite = $this->accounting_report_types_model->get_favorite_report_by_report_type_id($reportType->id, logged('company_id')); ?>
+                                                    <li class="p-3 cursor-pointer">
+                                                        <span
+                                                            onclick="location.href='<?php echo is_null($reportType->url) ? base_url('/accounting/reports/view-report/'.$reportType->id) : base_url($reportType->url); ?>'"><?php echo $reportType->name; ?></span>
+                                                        <a href="#" style="color: #888888" data-bs-toggle="collapse"
+                                                            data-bs-target="#<?php echo str_replace(' ', '-', strtolower($reportGroup->description)); ?>-<?php echo $reportType->id; ?>-collapse"
+                                                            aria-expanded="false"
+                                                            aria-controls="<?php echo str_replace(' ', '-', strtolower($reportGroup->description)); ?>-<?php echo $reportType->id; ?>-collapse">
+                                                            <i class="bx bx-fw bx-help-circle report-help-popover" data-toggle="popover" data-bs-content="<?= $reportType->description; ?>"></i>
+                                                        </a>
+
+                                                        <div class="dropdown float-end d-inline-block"
+                                                            style="min-width: 23px; min-height: 1px">                                                            
+                                                            <a href="#" class="dropdown-toggle"
+                                                                data-bs-toggle="dropdown" style="color: #888888">
+                                                                <i class='bx bx-fw bx-dots-vertical-rounded'></i>
+                                                            </a>
+                                                            <ul class="dropdown-menu dropdown-menu-end">                                                                
+                                                                <li><a class="dropdown-item btn-add-to-management-reports" href="javascript:void(0);" data-name="<?= $reportType->name; ?>" data-id="<?= $reportType->id; ?>">Add to Management Reports</a></li>
+                                                            </ul>                                                            
+                                                        </div>
+                                                    </li>
+                                                    <?php } ?>
+                                                </ul>
+                                            </div>
+                                            <?php } ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>                
+                </div>
             </div>
         </div>
     </div>
 </div>
+<script>
+$(function(){
+    $('.report-help-popover').popover({
+        placement: 'top',
+        html : true, 
+        trigger: "hover focus"
+    }); 
 
+    $('.btn-remove-management-reports').on('click', function(){
+        var rid = $(this).attr('data-id');
+        var report_name = $(this).attr('data-name');
+        var url = base_url + 'accounting/reports/_remove_management_reports';
+
+        Swal.fire({
+            title: 'Management Reports',
+            html: `Proceed with removing <b>${report_name}</b> report in management report list?`,
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'No',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    data: {rid:rid},
+                    dataType:'json',
+                    success: function(result) {
+                        if( result.is_success == 1 ) {
+                            Swal.fire({
+                            icon: 'success',
+                            title: 'Success',
+                            text: 'Report was successfully removed',
+                            }).then((result) => {
+                                window.location.reload();
+                            });
+                        } else {
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Error',
+                                text: result.msg,
+                            });
+                        }
+                    }
+                });
+            }
+        });
+    });
+});
+</script>
 <?php include viewPath('v2/includes/footer'); ?>
