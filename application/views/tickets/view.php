a6440be422eca973be4a5faa2f2aae7864d212c0
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-09-09 17:23:03 +0800

    Fixed date format

--- application/views/tickets/view.php
+++ application/views/tickets/view.php
@@ -107,7 +107,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
                             </tr>
                             <tr style="font-weight:;">
                                 <td>Scheduled Date:</td>
-                                <td style="text-align:right;"><?php echo date("m-d-Y", strtotime($tickets->ticket_date) );  //$tickets->ticket_date; ?></td>
+                                <td style="text-align:right;">
+                                    <?php 
+                                        $date = '---';
+                                        if( strtotime($tickets->ticket_date) > 0 ){
+                                            $date =  date("m/d/Y", strtotime($tickets->ticket_date)); 
+                                        }
+                                    ?>
+                                    <?php echo $date; ?>
+                                </td>
                             </tr>
                             <tr style="font-weight:;">
                                 <td>Scheduled Time:</td>
