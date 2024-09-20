1a3d6151820866ff12e0cf73f8870ce65d62cf24
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-09-20 12:14:56 +0800

    Added timesheet

--- application/views/v2/includes/page_navigations/employees_tabs.php
+++ application/views/v2/includes/page_navigations/employees_tabs.php
@@ -7,7 +7,16 @@
                 <span>Employees</span>
             </a>
         </li>
-        <li class="<?php if ($page->title == 'Attendance' || $page->title == 'My Schedule' || $page->title == 'Notification' || $page->title == 'Attendance Logs' || $page->title == 'Time Employee' || $page->title == 'Time Schedule' || $page->title == 'Requests' || $page->title == 'Shift Schedule' || $page->title == 'Timesheet Settings'): echo 'active';
+        <?php if (logged('user_type') == 7) { ?>
+            <li class="<?php if ($page->title == 'Time Employee'): echo 'active';
+                    endif; ?>">
+                <a class="nsm-page-link" href="<?php echo base_url('timesheet/employee') ?>">
+                    <i class='bx bx-fw bx-time'></i>
+                    <span>Timesheet</span>
+                </a>
+            </li>
+        <?php } ?>
+        <li class="<?php if ($page->title == 'Attendance' || $page->title == 'My Schedule' || $page->title == 'Notification' || $page->title == 'Attendance Logs' || $page->title == 'Time Schedule' || $page->title == 'Requests' || $page->title == 'Shift Schedule' || $page->title == 'Timesheet Settings'): echo 'active';
                     endif; ?>">
             <div class="dropdown" id="test_dropdown">
                 <a class="nsm-page-link dropdown-toggle" role="button" href="javascript:void(0);">
@@ -17,8 +26,7 @@
                     <i class='bx bx-fw bx-chevron-down dropdown-icon'></i>
                 </a>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="<?php echo base_url('timesheet/attendance') ?>">Attendance</a></li>
-
+                    <li><a class="dropdown-item" href="<?php echo base_url('timesheet/attendance') ?>">Attendance</a></li>                    
                     <?php if (logged("role") < 5): ?>
                         <li><a class="dropdown-item" href="<?php echo base_url('timesheet/attendance_logs') ?>">Time Logs</a></li>
                         <li><a class="dropdown-item" href="<?php echo base_url('timesheet/notification') ?>">Notification</a></li>
