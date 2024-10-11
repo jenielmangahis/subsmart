b31490acd3ddfb2c29f692630b928ef8631f44b6
Bryann Revina <bryann.revina03@gmail.com>
2024-10-11 14:24:43 +0800

    fixes

--- application/views/v2/pages/customer/advance_customer_forms/preview_customer_info.php
+++ application/views/v2/pages/customer/advance_customer_forms/preview_customer_info.php
@@ -310,40 +310,67 @@
         <div class="row g-1">
             <div class="col-12 col-md-6">
                 <label class="content-subtitle fw-bold">Billing Method</label>
-            </div>
+            </div>            
             <div class="col-12 col-md-6">
                 <label class="content-subtitle"><?php $bm = bill_methods($billing_info->bill_method); ?> <?= !empty($bm['description']) ? $bm['description'] : '---'; ?></label>
             </div>
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CHECK' && $billing_info->bill_method != 'ACH' ? 'display:none;' : ''; ?>">
                 <label class="content-subtitle fw-bold">Check Number</label>
             </div>
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CHECK' && $billing_info->bill_method != 'ACH' ? 'display:none;' : ''; ?>">
                 <label class="content-subtitle"><?= !empty($billing_info->check_num) ? $billing_info->check_num : '---' ?></label>
             </div>
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CHECK' && $billing_info->bill_method != 'ACH' ? 'display:none;' : ''; ?>">
                 <label class="content-subtitle fw-bold">Routing Number</label>
             </div>
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CHECK' && $billing_info->bill_method != 'ACH' ? 'display:none;' : ''; ?>">
                 <label class="content-subtitle"><?= !empty($billing_info->routing_num) ? $billing_info->routing_num : '---' ?></label>
             </div>
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CHECK' && $billing_info->bill_method != 'ACH' ? 'display:none;' : ''; ?>">
                 <label class="content-subtitle fw-bold">Account Number</label>
             </div>
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CHECK' && $billing_info->bill_method != 'ACH' ? 'display:none;' : ''; ?>">
                 <label class="content-subtitle"><?= !empty($billing_info->acct_num) ? $billing_info->acct_num : '---' ?></label>
             </div>
-            <div class="col-12 col-md-6">
+
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CHECK' && $billing_info->bill_method != 'ACH' ? 'display:none;' : ''; ?>">
+                <label class="content-subtitle fw-bold">Bank Name</label>
+            </div>
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CHECK' && $billing_info->bill_method != 'ACH' ? 'display:none;' : ''; ?>">
+                <label class="content-subtitle"><?= !empty($billing_info->bank_name) ? $billing_info->bank_name : '---' ?></label>
+            </div>
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CC' ? 'display:none;' : ''; ?>">
+                <label class="content-subtitle fw-bold">Credit Card Type</label>
+            </div>
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CC' ? 'display:none;' : ''; ?>">
+                <label class="content-subtitle"><?= !empty($billing_info->card_type) ? $billing_info->card_type : '---' ?></label>
+            </div>
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CC' ? 'display:none;' : ''; ?>">
                 <label class="content-subtitle fw-bold">Credit Card Number</label>
             </div>
-            <div class="col-12 col-md-6">
-                <label class="content-subtitle"><?= !empty($billing_info->credit_card_num) ? $billing_info->credit_card_num : '---' ?></label>
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CC' ? 'display:none;' : ''; ?>">
+                <?php 
+                    if (logged("user_type") == 1){
+                        $cc_num = $billing_info->credit_card_num;
+                    }else{
+                        $cc_num = strMask($billing_info->credit_card_num,'X');
+                    }
+                ?>
+                <label class="content-subtitle"><?= !empty($billing_info->credit_card_num) ? $cc_num : '---' ?></label>
             </div>
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CC' ? 'display:none;' : ''; ?>">
                 <label class="content-subtitle fw-bold">Credit Card Expiration</label>
             </div>
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CC' ? 'display:none;' : ''; ?>">
                 <label class="content-subtitle">
-                    <?= !empty($billing_info->credit_card_exp) ? $billing_info->credit_card_exp : '---' ?>
+                    <?= !empty($billing_info->credit_card_exp) ? $billing_info->credit_card_exp : '---' ?>                    
+                </label>
+            </div>
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CC' ? 'display:none;' : ''; ?>">
+                <label class="content-subtitle fw-bold">Credit Card CVC</label>
+            </div>
+            <div class="col-12 col-md-6" style="<?= $billing_info->bill_method != 'CC' ? 'display:none;' : ''; ?>">
+                <label class="content-subtitle">
                     <?= !empty($billing_info->credit_card_exp_mm_yyyy) ? $billing_info->credit_card_exp_mm_yyyy : '---' ?>
                 </label>
             </div>
