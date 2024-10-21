67b02cd1b9cf3af509f76c13558269daff993854
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-10-21 16:20:55 +0800

    Ticket sub nav

---
+++ application/views/v2/includes/page_navigations/service_ticket_subtabs.php
@@ -0,0 +1,19 @@
+<div class="nsm-page-subnav">
+    <ul>
+        <li class="<?php if($page->title == 'Tickets'): echo 'active'; endif; ?>" onclick="location.href='<?= base_url('customer/ticketslist') ?>'">
+            <a class="nsm-page-link" href="javascript:void(0);">
+                <span>List</span>
+            </a>
+        </li>
+        <li class="<?php if($page->title == 'Panel Types'): echo 'active'; endif; ?>" onclick="location.href='<?= base_url('tickets/settings_panel_types') ?>'">
+            <a class="nsm-page-link" href="javascript:void(0);">
+                <span>Panel Types</span>
+            </a>
+        </li>
+        <li class="<?php if($page->title == 'Plan Types'): echo 'active'; endif; ?>" onclick="location.href='<?= base_url('tickets/settings_plan_types') ?>'">
+            <a class="nsm-page-link" href="javascript:void(0);">
+                <span>Plan Types</span>
+            </a>
+        </li>
+    </ul>
+</div>
