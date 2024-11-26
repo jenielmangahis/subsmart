654b9b4fa43106a99fb4fb59857c7b382de1ed5c
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-11-26 13:35:32 +0800

    Merged changes

--- application/views/v2/widgets/sales_leaderboard_details.php
+++ application/views/v2/widgets/sales_leaderboard_details.php
@@ -11,7 +11,7 @@
 }
 </style>
 <?php if (count($salesLeaderBoards) > 0) : ?>
-    <table class="nsm-table table table table-borderless" id="widget-sales-leaderboard">
+    <table class="table table table-borderless" id="widget-sales-leaderboard">
         <thead>
         <tr>
             <td>Employee</td>            
