b98458906b9419403e9537c4d7a43d4648a89ccc
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-12-05 22:29:59 +0800

    New model files

---
+++ application/models/ActivationFee_model.php
@@ -0,0 +1,57 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class ActivationFee_model extends MY_Model
+{
+    public $table = 'ac_activationfee';
+
+    public function getById($id)
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('id', $id);
+
+        $query = $this->db->get();
+        return $query->row();
+    }
+
+    public function getByIdAndCompanyId($id, $cid)
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('id', $id);
+        $this->db->where('company_id', $cid);
+
+        $query = $this->db->get();
+        return $query->row();
+    }
+
+    public function getAll()
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getAllByCompanyId($cid, $conditions = [])
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('company_id', $cid);
+
+        if( !empty($conditions) ){
+            $this->db->group_start();            
+            foreach( $conditions as $c ){
+                $this->db->or_like($c['field'], $c['value']);    
+            }
+            $this->db->group_end();
+        }
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+}
+
+/* End of file ActivationFee_model.php */
+/* Location: ./application/models/ActivationFee_model.php */
---
+++ application/models/RatePlan_model.php
@@ -0,0 +1,68 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class RatePlan_model extends MY_Model
+{
+    public $table = 'ac_rateplan';
+
+    public function getById($id)
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('id', $id);
+
+        $query = $this->db->get();
+        return $query->row();
+    }
+
+    public function getByIdAndCompanyId($id, $cid)
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('id', $id);
+        $this->db->where('company_id', $cid);
+
+        $query = $this->db->get();
+        return $query->row();
+    }
+
+    public function getByNameAndCompanyId($name, $cid)
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('plan_name', $name);
+        $this->db->where('company_id', $cid);
+
+        $query = $this->db->get();
+        return $query->row();
+    }
+
+    public function getAll()
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getAllByCompanyId($cid, $conditions = [])
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('company_id', $cid);
+
+        if( !empty($conditions) ){
+            $this->db->group_start();            
+            foreach( $conditions as $c ){
+                $this->db->or_like($c['field'], $c['value']);    
+            }
+            $this->db->group_end();
+        }
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+}
+
+/* End of file RatePlan_model.php */
+/* Location: ./application/models/RatePlan_model.php */
