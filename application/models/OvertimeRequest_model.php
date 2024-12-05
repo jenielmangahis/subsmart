715a7375f50574dd695fd797890e3af3a38f4074
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-12-05 10:03:18 +0800

    New model files

---
+++ application/models/OvertimeRequest_model.php
@@ -0,0 +1,109 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class OvertimeRequest_model extends MY_Model
+{
+    public $table = 'timesheet_overtime_requests';
+    public $request_pending  = 1;
+    public $request_approved = 2;
+    public $request_disapproved = 3;
+
+    public function getById($id)
+    {
+        $this->db->select('timesheet_overtime_requests.*,CONCAT(users.FName, " ", users.LName)AS employee');
+        $this->db->from($this->table);
+        $this->db->join('users', 'timesheet_overtime_requests.user_id = users.id', 'left');
+        $this->db->where('timesheet_overtime_requests.id', $id);
+
+        $query = $this->db->get();
+        return $query->row();
+    }
+
+    public function getByIdAndCompanyId($id, $cid)
+    {
+        $this->db->select('timesheet_overtime_requests.*,CONCAT(users.FName, " ", users.LName)AS employee');
+        $this->db->from($this->table);
+        $this->db->join('users', 'timesheet_overtime_requests.user_id = users.id', 'left');
+        $this->db->where('timesheet_overtime_requests.id', $id);
+        $this->db->where('users.company_id', $cid);
+
+        $query = $this->db->get();
+        return $query->row();
+    }
+
+    public function getAll()
+    {
+        $this->db->select('timesheet_overtime_requests.*,CONCAT(users.FName, " ", users.LName)AS employee');
+        $this->db->from($this->table);
+        $this->db->join('users', 'timesheet_overtime_requests.user_id = users.id', 'left');
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getAllByCompanyId($cid, $conditions = [])
+    {
+        $this->db->select('timesheet_overtime_requests.*,CONCAT(users.FName, " ", users.LName)AS employee');
+        $this->db->from($this->table);
+        $this->db->join('users', 'timesheet_overtime_requests.user_id = users.id', 'left'); 
+        $this->db->where('users.company_id', $cid);
+
+        if( !empty($conditions) ){
+            $this->db->group_start();            
+            foreach( $conditions as $c ){
+                $this->db->or_like($c['field'], $c['value']);    
+            }
+            $this->db->group_end();
+        }
+
+        $query = $this->db->get();
+        return $query->result();
+    }   
+    
+    public function getAllByUserId($uid, $conditions = [])
+    {
+        $this->db->select('timesheet_overtime_requests.*,CONCAT(users.FName, " ", users.LName)AS employee');
+        $this->db->from($this->table);
+        $this->db->join('users', 'timesheet_overtime_requests.user_id = users.id', 'left');
+        $this->db->where('users.id', $uid);
+
+        if( !empty($conditions) ){
+            $this->db->group_start();            
+            foreach( $conditions as $c ){
+                $this->db->or_like($c['field'], $c['value']);    
+            }
+            $this->db->group_end();
+        }
+
+        $query = $this->db->get();
+        return $query->result();
+    }   
+
+    public function requestStatusPending()
+    {
+        return $this->request_pending;
+    }
+
+    public function requestStatusApproved()
+    {
+        return $this->request_approved;
+    }
+
+    public function requestStatusDisApproved()
+    {
+        return $this->request_disapproved;
+    }
+
+    public function optionStatus()
+    {
+        $options = [
+            $this->request_approved => 'Approve',
+            $this->request_disapproved => 'Disapprove'
+        ];
+
+        return $options;
+    }
+}
+
+/* End of file OvertimeRequest_model.php */
+/* Location: ./application/models/OvertimeRequest_model.php */
