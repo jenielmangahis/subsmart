156a2f10c6d7f5a777a15b6705caaa5e04d1332e
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-12-05 22:40:47 +0800

    Merged changes

--- application/views/v2/pages/customer/advance_customer_forms/quick_add_modal_forms.php
+++ application/views/v2/pages/customer/advance_customer_forms/quick_add_modal_forms.php
@@ -32,6 +32,39 @@
     </div>
 </div>
 
+<div class="modal fade" id="quick_add_transaction_category" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <span class="modal-title content-title" style="font-size: 17px;">Create Transaction Category</span>
+                <i class="bx bx-fw bx-x m-0 text-muted" data-bs-dismiss="modal" aria-label="name-button" name="name-button" style="cursor: pointer;"></i>
+            </div>
+            <div class="modal-body">
+                <form id="frm-quick-add-transaction-category">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <label class="mb-2">Name</label>
+                            <div class="input-group mb-3">
+                                <input type="text" name="category_name" value="" class="form-control" required="" autocomplete="off" />
+                            </div>
+                        </div>
+                        <div class="col-sm-12">
+                            <label class="mb-2">Value</label>
+                            <div class="input-group mb-3">
+                                <input type="text" name="category_value" value="" class="form-control" required="" autocomplete="off" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end">                        
+                        <button type="button" id="" class="nsm-button" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="nsm-button primary" id="btn-save-transaction-category">Save</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="modal fade" id="quick_add_customer_status" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
