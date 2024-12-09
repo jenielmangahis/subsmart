588d1ab0d439113ed61d137f1c0e9a6a7bccff66
Bryann Revina <bryann.revina03@gmail.com>
2024-12-09 21:45:14 +0800

    Changes

--- application/views/v2/includes/page_navigations/invoce_tabs_v2.php
+++ application/views/v2/includes/page_navigations/invoce_tabs_v2.php
@@ -1,11 +1,27 @@
 <div class="nsm-page-nav">
     <ul>
-        <li class="<?php if($page->title == 'Invoices & Payments' || $page->title == 'Recurring Invoices' || $page->title == 'Tax Rates' || $page->title == 'Invoice Settings'): echo 'active'; endif; ?>">
+        <li class="<?php if($page->title == 'Invoices & Payments' || $page->title == 'Recurring Invoices'): echo 'active'; endif; ?>">
             <a class="nsm-page-link" href="<?= base_url('invoice') ?>">
                 <i class='bx bx-fw bx-receipt'></i>
                 <span>Invoices</span>
             </a>
         <!-- Do not remove the last li -->
+        <?php //if (logged('user_type') == 7) { ?>
+        <li class="<?php if($page->title == 'Invoice Settings' || $page->title == 'Tax Rates' || $page->title == 'Payment Terms'): echo 'active'; endif; ?>">
+            <div class="dropdown" id="test_dropdown">
+                <a class="nsm-page-link dropdown-toggle" role="button" href="javascript:void(0);">
+                    <i class='bx bx-fw bx-cog'></i>
+                    <span>Invoice Settings</span>
+                    <i class='bx bx-fw bx-chevron-down dropdown-icon'></i>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="<?= base_url('invoice/settings') ?>">General Settings</a></li>
+                    <li><a class="dropdown-item" href="<?php echo base_url('settings/tax_rates') ?>">Tax Rates</a></li>
+                    <li><a class="dropdown-item" href="<?php echo base_url('accounting/terms') ?>">Payment Terms</a></li>                                        
+                </ul>
+            </div>
+        </li>
+        <?php //} ?>
         <li><label></label></li>
     </ul>
 </div>
