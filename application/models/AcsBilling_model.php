912c157e145f8dc9f645eece8538f6b3be345fab
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-12-11 10:02:33 +0800

    Fixes / Changes

--- application/models/AcsBilling_model.php
+++ application/models/AcsBilling_model.php
@@ -14,5 +14,43 @@ class AcsBilling_model extends MY_Model
         $query = $this->db->get();
         return $query->result();
     }
+
+    public function getAll($limit = 0)
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);        
+        
+        if ($limit > 0) {
+            $this->db->limit($limit);
+        }
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    // This method needs is_checked field.
+    // Only used in correcting records
+    public function getAllNotChecked($limit = 0)
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('is_checked', 0);
+
+        if ($limit > 0) {
+            $this->db->limit($limit);
+        }
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function updateRecord($bill_id, $data)
+    {
+        $this->db->where('bill_id', $bill_id);
+        $this->db->from($this->table);
+        $cust = $this->db->update($this->table, $data);
+
+        return $cust;
+    }
 }
 ?>
