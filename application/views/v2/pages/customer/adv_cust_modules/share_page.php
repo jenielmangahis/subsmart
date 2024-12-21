192ad1cfa22f3d80c73400fdc9453a65414ac694
Bryann Revina <bryann.revina03@gmail.com>
2024-12-21 14:14:08 +0800

    Merged changes

--- application/views/v2/pages/customer/adv_cust_modules/share_page.php
+++ application/views/v2/pages/customer/adv_cust_modules/share_page.php
@@ -1,4 +1,4 @@
-<div class="col-12 col-md-8">
+<div class="col-12 col-md-4">
     <div class="nsm-card primary">
         <div class="nsm-card-header"><b><i class="bx bx-fw bxs-share" style="position:relative;top:1px;"></i> Share Login Page</b></div>
         <div class="nsm-card-content">
@@ -8,16 +8,18 @@
                     $customer_unique_id = hashids_encrypt($cus_id, '', 45);
                     $public_url = base_url('/client_hub/' . $customer_unique_id . "?source=share");
                 ?>
-                <div class="input-group rounded">
+                <div class="col-md-12">
+                    <label class="visually-hidden" for="customer-public-url">Username</label>
+                    <div class="input-group">
+                    <div class="input-group-text"><i class='bx bxs-user-rectangle'></i></div>
+                        <input type="text" class="form-control customer-public-url" id="customer-public-url" placeholder="Customer Hub" disabled value="<?php echo $public_url; ?>" >                        
+                    </div>
+                    <button class="nsm-button primary w-40 ms-0 copy-customer-public-url mt-2" id="copy-customer-public-url" onClick="javascript:copyCustomerPublicUrl();" style="background-color: #6a4a86; color: #ffffff;"><i class='bx bx-copy-alt'></i> Copy to clipboard</button>  
+                </div>
+                <!-- <div class="input-group rounded">
                     <button class="input-group-text border-0 copy-customer-public-url" id="copy-customer-public-url" onClick="javascript:copyCustomerPublicUrl();" style="background-color: #6a4a86; color: #ffffff;"><strong>COPY URL</strong></button>    
                     <input type="text" class="form-control rounded customer-public-url" id="customer-public-url" placeholder="" disabled value="<?php echo $public_url; ?>" >
-                </div>     
-   
-                <div class="input-group rounded">
-                <div id="url-copied-container" class="nsm primary url-copied-container" style="background-color: #6ba77c21; width: 100%; margin-top: 5px; display:none;">Url copied..</div>      
-                <div id="url-copied-container-fail" class="nsm warning url-copied-container-fail" style="background-color: #dc3545; color:#ffffff; width: 100%; margin-top: 5px; display:none;">Url copy fail..</div>      
-                </div>
-              
+                </div>        -->
             </div>
         </div>
     </div>
@@ -28,9 +30,21 @@ $(function(){
     $(document).on('click', '.copy-customer-public-url', function(){
         const copyUrl = $("#customer-public-url").val();
         navigator.clipboard.writeText(copyUrl).then(() => {
-            $("#url-copied-container").show();
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "URL Copied to clipboard",
+                showConfirmButton: false,
+                timer: 1000
+            });
         }).catch(() => {
-            $("#url-copied-container-fail").show();
+            Swal.fire({
+                position: "top-end",
+                icon: "error",
+                title: "Cannot copy url",
+                showConfirmButton: false,
+                timer: 1000
+            });
         })
     });
 });
