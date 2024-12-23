3bb9d13440b4a5f768d5baa7c2c51051ad9b1080
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-12-23 14:07:50 +0800

    New model file

---
+++ application/models/CompanyRoleAccessModule_model.php
@@ -0,0 +1,135 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class CompanyRoleAccessModule_model extends MY_Model
+{
+    public $table = 'company_role_access_modules';
+
+    public function getAll($filters=array())
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+
+        if ( !empty($filters) ) {
+            if ( !empty($filters['search']) ) {
+                $this->db->like('offer_code', $filters['search'], 'both');
+            }
+        }
+
+        $this->db->order_by('id', 'ASC');
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getById($id)
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('id', $id);
+
+        $query = $this->db->get()->row();
+        return $query;
+    }
+
+    public function getAllByCompanyId($cid)
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('company_id', $cid);
+        $this->db->order_by('id', 'DESC');
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getAllByCompanyIdAndRoleId($cid, $role_id)
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('company_id', $cid);
+        $this->db->where('role_id', $role_id);
+        $this->db->order_by('id', 'DESC');
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getAllRolesByCompanyId($cid)
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('company_id', $cid);
+        $this->db->order_by('id', 'DESC');
+        $this->db->group_by('role_id');
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getCompanyRoleModulePermission($cid, $rid, $module)
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('company_id', $cid);
+        $this->db->where('role_id', $rid);
+        $this->db->where('module', $module);
+        $this->db->order_by('id', 'DESC');
+        
+        $query = $this->db->get()->row();
+        return $query;
+    }
+
+    public function isRoleHasAccessAll($cid, $rid)
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('company_id', $cid);
+        $this->db->where('role_id', $rid);
+        $this->db->where('module', 'access-all');
+        $this->db->order_by('id', 'DESC');
+        
+        $query = $this->db->get()->row();
+        return $query;
+    }
+
+    public function deleteAllByCompanyIdAndRoleId($cid, $rid)
+    {
+        $this->db->delete($this->table, ['company_id' => $cid, 'role_id' => $rid]);
+    }
+    
+    public function modules()
+    {
+        $modules = [
+            'dashboard' => 'Dashboard',
+            'customers' => 'Customers',
+            'users' => 'Users',
+            'user-timesheet' => 'Timesheet',
+            'user-settings' => 'User Settings',
+            'user-track-location' => 'Employees Track Location',
+            'user-payscale' => 'Pay Scale',
+            'my-business' => 'My Business',
+            'calendar-schedule' => 'Calendar Schedule',
+            'calendar-settings' => 'Calendar Settings',
+            'customer-groups' => 'Customer Groups',
+            'customer-settings' => 'Customer Settings',            
+            'leads' => 'Leads',
+            'events' => 'Events',
+            'events-settings' => 'Events Settings',
+            'jobs' => 'Jobs',
+            'job-setitngs' => 'Job Settings', 
+            'service-tickets' => 'Service Tickets',
+            'estimates' => 'Estimates',
+            'work-orders' => 'Work Orders',
+            'invoices' => 'Invoices',
+            'invoice-settings' => 'Invoice Settings',
+            'accounting-banking' => 'Accounting Banking',
+            'accounting-link-bank' => 'Accounting Link Bank'
+        ];  
+
+        return $modules;
+    }
+
+}
+
+/* End of file CompanyRoleAccessModule_model.php */
+/* Location: ./application/models/CompanyRoleAccessModule_model.php */
