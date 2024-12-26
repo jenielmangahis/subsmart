746038cb673344e3b57a0209285d1811a2808d40
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-12-26 09:56:43 +0800

    Merged changes

--- application/controllers/ClientHub.php
+++ application/controllers/ClientHub.php
@@ -6,38 +6,63 @@ class ClientHub extends MYF_Controller {
 		parent::__construct();
 		$this->page_data['page']->title = 'nSmart - Customer Public Portal';
 
-		$this->load->model('Jobs_model', 'jobs_model');
+		$this->load->helper(array('hashids_helper'));
+		$this->load->model('Jobs_model');
+		$this->load->model('Tickets_model');
+		$this->load->model('AcsProfile_model');
+		$this->load->model('Business_model');
 	}
 
 	public function index($id){	
-		$this->load->helper(array('hashids_helper'));
+		$this->page_data['page']->portal_tabs = 'portal_customer_information';
+		$this->page_data['customer_id_incrypt'] = $id;
+		$customer_id = hashids_decrypt($id, '', 45);
 
+		$profile_info = $this->AcsProfile_model->getByProfId($customer_id);
+		$this->page_data['profile_info'] = $profile_info;
+		$this->load->view('v2/pages/customer/client_hub/profile', $this->page_data);
+	}
+
+	public function jobs($id){	
 		$this->page_data['page']->portal_tabs = 'portal_jobs';
 		$this->page_data['customer_id_incrypt'] = $id;
 		$customer_id = hashids_decrypt($id, '', 45);
 
-		$jobs = $this->jobs_model->getAllJobsByCustomerId($customer_id);
+		$jobs = $this->Jobs_model->getAllJobsByCustomerId($customer_id);
 		$this->page_data['jobs'] = $jobs;
-		$this->load->view('v2/pages/customer/client_hub/index', $this->page_data);
+		$this->load->view('v2/pages/customer/client_hub/jobs', $this->page_data);
 	}
 
 	public function tickets($id){	
-		$this->load->helper(array('hashids_helper'));
-
 		$this->page_data['page']->portal_tabs = 'portal_tickets';
 		$this->page_data['customer_id_incrypt'] = $id;
 
 		$customer_id = hashids_decrypt($id, '', 45);
+		$tickets = $this->Tickets_model->getAllByCustomerId($customer_id);
+
+		$this->page_data['tickets'] = $tickets;
 		$this->load->view('v2/pages/customer/client_hub/tickets', $this->page_data);
 	}
 
 	public function invoice_status($id){	
-		$this->load->helper(array('hashids_helper'));
-
 		$this->page_data['page']->portal_tabs = 'portal_invoice_status';
 		$this->page_data['customer_id_incrypt'] = $id;
 		
 		$customer_id = hashids_decrypt($id, '', 45);
 		$this->load->view('v2/pages/customer/client_hub/invoice_status', $this->page_data);
 	}
+
+	public function customer_profile(){
+		$customer_id = hashids_decrypt($id, '', 45);
+	}
+
+	public function ajax_view_customer_job()
+    {
+        $post     = $this->input->post();
+		$id       = hashids_decrypt($post['jobid'], '', 45);
+		$job      = $this->Jobs_model->get_specific_job($id);
+		$business = $this->Business_model->getByCompanyId($job->company_id);
+
+        
+    }
 }
