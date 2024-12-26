efa590f2b2e7a71c4997dad88c38bdaa7b9c0277
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2024-12-26 09:58:48 +0800

    Changes

--- application/views/v2/pages/customer/client_hub/index.php
+++ application/views/v2/pages/customer/client_hub/index.php
@@ -59,107 +59,13 @@
                             For any business, getting customers is only half the battle; creating a job workflow will help track each scheduled ticket from draft to receiving payment.
                         </div>
                     </div>
-                </div>
+                </div>   
 
                 <div class="row">
-                    <div class="col-6 grid-mb">
-                        <form action="<?php echo base_url('customer/status') ?>" method="get">
-                            <div class="nsm-field-group search">
-                                <input type="text" class="nsm-field nsm-search form-control mb-2" id="search_field" name="search" placeholder="Search" value="<?php echo (!empty($search)) ? $search : '' ?>">
-                            </div>
-                        </form>   
-                    </div>                      
-                    <div class="col-6 grid-mb text-end">
-                        <div class="nsm-page-buttons page-button-container"></div>
-                    </div>
-                </div>                
-
-                <div class="row">
-                    <table class="nsm-table">
-                        <thead>
-                            <tr>
-                                <td class="table-icon"></td>
-                                <td data-name="Job Number">Job Number</td>
-                                <td data-name="Date">Date</td>
-                                <td data-name="Customer">Customer</td>
-                                <td data-name="Status">Status</td>
-                                <td data-name="Amount">Job Amount</td>
-                                <td data-name="Priority">Priority</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        <?php foreach ($jobs as $job) { ?>
-                            <?php 
-                                switch($job->status):
-                                    case "New":
-                                        $badgeCount = 1;
-                                        break;
-                                    case "Scheduled":
-                                        $badgeCount = 2;
-                                        break;
-                                    case "Arrival":
-                                        $badgeCount = 3;
-                                        break;          
-                                    case "Started":
-                                        $badgeCount = 4;
-                                        break;
-                                    case "Approved":
-                                        $badgeCount = 5;
-                                        break;
-                                    case "Closed":
-                                        $badgeCount = 6;
-                                        break;
-                                    case "Invoiced":
-                                        $badgeCount = 7;
-                                        break;
-                                    case "Completed":
-                                    case "Finished":
-                                        $badgeCount = 8;
-                                        break;
-                                endswitch;                                
-                            ?>                            
-                            <tr>
-                                <td>
-                                    <div class="table-row-icon">
-                                        <i class='bx bx-bar-chart-square'></i>
-                                    </div>
-                                </td>
-                                <td class="fw-bold nsm-text-primary">                       
-                                    <?= $job->job_number; ?>
-                                </td>
-                                <td><?php echo date_format(date_create($job->start_date), "m/d/Y"); ?></td>
-                                <td><?php echo $job->first_name . ' ' . $job->last_name; ?></td>
-                                <td>
-                                    <div>
-                                        <?php for($x=1;$x<=$badgeCount;$x++){ ?> 
-                                            <span class="nsm-badge primary-enhanced"></span>
-                                        <?php } for($y=1;$y < 9 - $badgeCount;$y++){ ?> 
-                                            <span class="nsm-badge primary"></span>
-                                        <?php } ?>
-                                    </div>
-                                    <small class="content-subtitle d-block mt-1"><?= $job->status; ?></small>                                    
-                                </td>
-                                <td>$<?php echo number_format((float)$job->amount, 2, '.', ',');  ?></td>
-                                <td><?php echo $job->priority; ?></td>
-                            </tr>
-                        <?php } ?>
-                        </tbody>
-                    </table>                    
+                                     
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-<?php include viewPath('customer/adv_cust/js_list'); ?>
-<script type="text/javascript">
-    $(document).ready(function() {
-        $(".nsm-table").nsmPagination();
-
-        $("#search_field").on("input", debounce(function() {
-            tableSearch($(this));        
-        }, 1000));       
-    });
-</script>
-
 <?php include viewPath('v2/includes/footer_clienthub'); ?>
