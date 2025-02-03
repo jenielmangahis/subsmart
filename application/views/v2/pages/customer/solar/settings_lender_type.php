cbaa44d9ef9fff77420ea4c1ad490c202d2d0aff
Bryann Revina <bryann.revina03@gmail.com>
2025-02-03 17:38:32 +0800

    Merged changes

--- application/views/v2/pages/customer/solar/settings_lender_type.php
+++ application/views/v2/pages/customer/solar/settings_lender_type.php
@@ -1,8 +1,7 @@
 <?php include viewPath('v2/includes/header'); ?>
-<?php include viewPath('v2/includes/customer/customer_modals'); ?>
 
 <div class="nsm-fab-container">
-    <div class="nsm-fab nsm-fab-icon nsm-bxshadow" data-bs-toggle="modal" data-bs-target="#new_lead_source_modal">
+    <div class="nsm-fab nsm-fab-icon nsm-bxshadow" data-bs-toggle="modal" data-bs-target="#new_lender_type_modal">
         <i class="bx bx-plus"></i>
     </div>
 </div>
@@ -24,19 +23,26 @@
                     </div>
                 </div>
                 <div class="row">
+                    <div class="col-12 col-md-4 grid-mb">
+                        <div class="nsm-field-group search">
+                            <input type="text" class="nsm-field nsm-search form-control mb-2" id="search_field" placeholder="Search">
+                        </div>
+                    </div>
+                    <?php if(checkRoleCanAccessModule('customer-settings', 'write')){ ?>
                     <div class="col-12 grid-mb text-end">
                         <div class="nsm-page-buttons page-button-container">
                             <button type="button" class="nsm-button primary" data-bs-toggle="modal" data-bs-target="#new_lender_type_modal">
-                                <i class='bx bx-fw bx-notepad'></i> New Lender Type
+                                <i class='bx bx-fw bx-plus'></i> New Lender Type
                             </button>
                         </div>
                     </div>
+                    <?php } ?>
                 </div>
                 <table class="nsm-table">
                     <thead>
                         <tr>
                             <td class="table-icon"></td>
-                            <td data-name="Name">Name</td>
+                            <td data-name="Name" style="width:80%;">Name</td>
                             <td data-name="Date Created">Date Created</td>
                             <td data-name="Manage"></td>
                         </tr>
@@ -55,7 +61,7 @@
                                         </div>
                                     </td>
                                     <td class="fw-bold nsm-text-primary"><?= $type->name; ?></td>
-                                    <td><?= date("d-m-Y h:i A",strtotime($type->date_created)); ?></td>
+                                    <td><?= date("m/d/Y h:i A",strtotime($type->date_created)); ?></td>
                                     <td>
                                         <div class="dropdown table-management">
                                             <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown">
@@ -63,8 +69,11 @@
                                             </a>
                                             <ul class="dropdown-menu dropdown-menu-end">
                                                 <li>
-                                                    <a class="dropdown-item delete-item" href="javascript:void(0);" data-id="<?= $type->name; ?>">Delete</a>
+                                                    <a class="dropdown-item edit-item" href="javascript:void(0);" data-id="<?= $type->id; ?>"  data-name="<?= $type->name; ?>">Edit</a>
                                                 </li>
+                                                <li>
+                                                    <a class="dropdown-item delete-item" href="javascript:void(0);" data-id="<?= $type->id; ?>" data-name="<?= $type->name; ?>">Delete</a>
+                                                </li>                                                
                                             </ul>
                                         </div>
                                     </td>
@@ -90,66 +99,169 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="new_lender_type_modal" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="modal-title content-title" style="font-size: 17px;">Create Lender Type</span>
+                    <i class="bx bx-fw bx-x m-0 text-muted" data-bs-dismiss="modal" aria-label="name-button" name="name-button" style="cursor: pointer;"></i>
+                </div>
+                <div class="modal-body">
+                    <form id="new_lender_type_form">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <label class="mb-2">Name</label>
+                                <div class="input-group mb-3">
+                                    <input type="text" name="lender_type_name" id="lender_type_name" value="" class="form-control" required="" autocomplete="off" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-end">                        
+                            <button type="button" id="" class="nsm-button" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="nsm-button primary btn-fixed-width" id="btn-save-lender-type">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="edit_lender_type_modal" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="modal-title content-title" style="font-size: 17px;">Edit Lender Type</span>
+                    <i class="bx bx-fw bx-x m-0 text-muted" data-bs-dismiss="modal" aria-label="name-button" name="name-button" style="cursor: pointer;"></i>
+                </div>
+                <div class="modal-body">
+                    <form id="edit_lender_type_form">
+                        <input type="hidden" name="lender_type_id" id="edit_lender_type_id" value="" />
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <label class="mb-2">Name</label>
+                                <div class="input-group mb-3">
+                                    <input type="text" name="lender_type_name" id="edit_lender_type_name" value="" class="form-control" required="" autocomplete="off" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-end">                        
+                            <button type="button" id="" class="nsm-button" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="nsm-button primary btn-fixed-width" id="btn-update-lender-type">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 <script type="text/javascript">
     $(document).ready(function() {
         $(".nsm-table").nsmPagination();
+        $("#search_field").on("input", debounce(function() {
+            tableSearch($(this));        
+        }, 1000));
 
         $(document).on("click", ".edit-item", function(){
             let id = $(this).attr("data-id");
             let name = $(this).attr("data-name");
 
-            $("#edit_ls_id").val(id);
-            $("#edit_ls_name").val(name);
+            $("#edit_lender_type_id").val(id);
+            $("#edit_lender_type_name").val(name);
 
-            $("#edit_lead_source_modal").modal("show");
+            $("#edit_lender_type_modal").modal("show");
         });
 
         $("#new_lender_type_form").on("submit", function(e) {
-            let _this = $(this);
             e.preventDefault();
 
-            var url = "<?php echo base_url(); ?>customer/settings_solar_lender_type";
-            _this.find("button[type=submit]").html("Saving");
-            _this.find("button[type=submit]").prop("disabled", true);
-
             $.ajax({
-                type: 'POST',
-                url: url,
-                data: _this.serialize(),
-                success: function(result) {
-                    if (result === "Updated") {
-                        
-                    } else {
+                type: "POST",
+                url: base_url + 'customers/_create_lender_type',
+                dataType: 'json',
+                data: $('#new_lender_type_form').serialize(),
+                success: function(data) {    
+                    $('#btn-save-lender-type').html('Save');                   
+                    if (data.is_success) {
+                        $('#new_lender_type_modal').modal('hide');
                         Swal.fire({
-                            title: 'Save Successful!',
-                            text: "New Lender Type has been added successfully.",
+                            title: 'Solar Lending Type',
+                            text: "Lending type has been created successfully.",
                             icon: 'success',
                             showCancelButton: false,
                             confirmButtonText: 'Okay'
                         }).then((result) => {
-                            if (result.value) {
-                                location.reload();
-                            }
+                            //if (result.value) {
+                                location.reload();  
+                            //}
+                        });
+                    }else{
+                        Swal.fire({
+                            title: 'Error',
+                            text: data.msg,
+                            icon: 'error',
+                            showCancelButton: false,
+                            confirmButtonText: 'Okay'
+                        }).then((result) => {
+                            
                         });
                     }
-                    $("#new_lender_type_modal").modal('hide');
-                    _this.trigger("reset");
-
-                    _this.find("button[type=submit]").html("Save");
-                    _this.find("button[type=submit]").prop("disabled", false);
                 },
+                beforeSend: function() {
+                    $('#btn-save-lender-type').html('<span class="bx bx-loader bx-spin"></span>');
+                }
             });
         });
 
+        $("#edit_lender_type_form").on("submit", function(e) {
+            e.preventDefault();
+
+            $.ajax({
+                type: "POST",
+                url: base_url + 'customers/_update_lender_type',
+                dataType: 'json',
+                data: $('#edit_lender_type_form').serialize(),
+                success: function(data) {    
+                    $('#btn-update-lender-type').html('Save');                   
+                    if (data.is_success) {
+                        $('#edit_lender_type_modal').modal('hide');
+                        Swal.fire({
+                            title: 'Solar Lending Type',
+                            text: "Lending type has been updated successfully.",
+                            icon: 'success',
+                            showCancelButton: false,
+                            confirmButtonText: 'Okay'
+                        }).then((result) => {
+                            //if (result.value) {
+                                location.reload();  
+                            //}
+                        });
+                    }else{
+                        Swal.fire({
+                            title: 'Error',
+                            text: data.msg,
+                            icon: 'error',
+                            showCancelButton: false,
+                            confirmButtonText: 'Okay'
+                        }).then((result) => {
+                            
+                        });
+                    }
+                },
+                beforeSend: function() {
+                    $('#btn-update-lender-type').html('<span class="bx bx-loader bx-spin"></span>');
+                }
+            });
+        });
         
         $(document).on("click", ".delete-item", function() {
             let id = $(this).attr("data-id");
-
+            let name = $(this).attr('data-name');
             Swal.fire({
                 title: 'Delete Lender Type',
-                text: "Are you sure you want to delete this Lender Type?",
+                html: `Are you sure you want to delete lender type <b>${name}</b>?`,
                 icon: 'question',
                 confirmButtonText: 'Proceed',
                 showCancelButton: true,
@@ -158,22 +270,31 @@
                 if (result.value) {
                     $.ajax({
                         type: 'POST',
-                        url: "<?php echo base_url(); ?>customer/settings_solar_lender_type",
-                        data: {
-                            lenderName : id
-                        },
-                        success: function(result) {
-                            if (result === '1') {
+                        url: base_url + "customers/_delete_lender_type",
+                        data: {lid : id},
+                        dataType:"json",
+                        success: function(data) {
+                            if (data.is_success) {
                                 Swal.fire({
-                                    title: 'Good job!',
+                                    title: 'Delete Lender Type',
                                     text: "Data Deleted Successfully!",
                                     icon: 'success',
                                     showCancelButton: false,
                                     confirmButtonText: 'Okay'
                                 }).then((result) => {
-                                    if (result.value) {
+                                    //if (result.value) {
                                         location.reload();
-                                    }
+                                    //}
+                                });
+                            }else{
+                                Swal.fire({
+                                    title: 'Error',
+                                    text: data.msg,
+                                    icon: 'error',
+                                    showCancelButton: false,
+                                    confirmButtonText: 'Okay'
+                                }).then((result) => {
+                                    
                                 });
                             }
                         },
