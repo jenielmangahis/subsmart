6c893b6e5b0f0048454b8d383bc1175ed555a8f0
Bryann Revina <bryann.revina03@gmail.com>
2025-02-10 17:40:24 +0800

    Updates (Jeniel - Feb 10 - Part 1) -Merge updates

--- application/models/Customer_advance_model.php
+++ application/models/Customer_advance_model.php
@@ -1470,6 +1470,20 @@ class Customer_advance_model extends MY_Model
         return $query->result();   
     }
 
+    public function getAllActiveSubscriptionsWithSub14DaysByCompanyId($current_date, $company_id) {
+
+        $this->db->select('acs_profile.*,acs_billing.*');
+        $this->db->from('acs_billing');
+        $this->db->join('acs_profile', 'acs_billing.fk_prof_id = acs_profile.prof_id', 'left');
+        $this->db->where('acs_profile.company_id =', $company_id);  
+        $this->db->where('CURDATE() >= DATE_SUB(acs_billing.next_billing_date, INTERVAL 14 DAY)');
+        $this->db->where('CURDATE() <= acs_billing.next_billing_date');   
+
+        $query = $this->db->get();
+
+        return $query->result();   
+    }
+
     public function getActiveSubscriptionsByCustomerId($customer_id)
     {
         $this->db->select('acs_profile.*,acs_billing.*');
