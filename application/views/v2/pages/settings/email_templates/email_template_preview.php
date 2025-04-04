d525bf627558cf22fca67abae215a74dd5d94e6d
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2025-04-04 22:14:26 +0800

    Added missing file

---
+++ application/views/v2/pages/settings/email_templates/email_template_preview.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nSmarTrac : Email Template</title>
+</head>
+<body>
+    <?= $emailTemplate->email_body; ?>
+</body>
+</html>
