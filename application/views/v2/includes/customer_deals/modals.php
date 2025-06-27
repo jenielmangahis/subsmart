7ea6bee55bdf6db6dfa734181e9ecf044cceb356
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2025-06-27 21:38:30 +0800

    Changes

--- application/views/v2/includes/customer_deals/modals.php
+++ application/views/v2/includes/customer_deals/modals.php
@@ -386,7 +386,7 @@
     <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <span class="modal-title content-title" style="font-size: 17px;">Archive</span>
+                <span class="modal-title content-title" style="font-size: 17px;">Archived</span>
                 <button class="border-0 rounded mx-1" data-bs-dismiss="modal" style="cursor: pointer;"><i class="fas fa-times m-0 text-muted"></i></button>
             </div>
             <div class="modal-body" id="customer-deals-archive-container"></div>            
