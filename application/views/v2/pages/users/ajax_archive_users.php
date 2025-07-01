86b80053ca08824e9a0df162aef97e025109b332
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2025-07-01 17:00:22 +0800

    Added archive script

---
+++ application/views/v2/pages/users/ajax_archive_users.php
@@ -0,0 +1,81 @@
+<div class="row">
+    <div class="col-12 grid-mb text-end">
+        <div class="dropdown d-inline-block">
+            <button type="button" class="nsm-button primary" id="btn-delete-all">Delete All</button>
+            <button type="button" class="dropdown-toggle nsm-button" data-bs-toggle="dropdown">
+                <span id="users-archive-num-checked"></span> With Selected  <i class='bx bx-fw bx-chevron-down'></i>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end select-filter">                 
+                <li><a class="dropdown-item btn-with-selected" id="with-selected-restore" href="javascript:void(0);">Restore</a></li>                                
+                <li><a class="dropdown-item btn-with-selected" id="with-selected-restore" href="javascript:void(0);">Permanently Delete</a></li>                                
+            </ul>
+        </div>
+    </div>
+</div>
+<form id="frm-archive-with-selected">
+<table class="nsm-table" id="archived-jobs">
+    <thead>
+        <tr>
+            <td class="table-icon text-center sorting_disabled">
+                <input class="form-check-input table-select" type="checkbox" name="" value="0" id="users-archive-select-all">
+            </td>
+            <td class="table-icon"></td>
+            <td data-name="UserName" style="width:40%;">Name</td>                        
+            <td data-name="Action" style="width:5%;"></td>                
+        </tr>
+    </thead>
+    <tbody>
+        <?php if ($users) { ?>
+            <?php foreach($users as $user){ ?>
+                <tr>
+                    <td class="text-center">
+                        <input class="form-check-input users-archive-row-select table-select" name="users[]" type="checkbox" value="<?= $user->id; ?>">
+                    </td>
+                    <td><div class="table-row-icon"><i class="bx bx-box"></i></div></td>
+                    <td class="nsm-text-primary"><?= $user->FName . ' ' . $user->LName; ?></td>
+                    <td style="width:5%;">
+                        <div class="dropdown table-management">
+                            <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown"><i class='bx bx-fw bx-dots-vertical-rounded'></i></a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item btn-restore-customer-deal" data-id="<?= $deal->id; ?>" data-title="<?= $deal->deal_title; ?>" href="javascript:void(0);"><i class='bx bx-recycle'></i> Restore</a></li>   
+                            </ul>
+                        </div>
+                    </td>
+                </tr>
+            <?php } ?>
+        <?php }else{ ?>
+            <tr>
+                <td colspan="5">
+                    <div class="nsm-empty">
+                        <span>No results found</span>
+                    </div>
+                </td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
+</form>
+<script>
+$(function(){
+    $("#archived-jobs").nsmPagination();
+
+    $(document).on('change', '#users-archive-select-all', function(){
+        $('.users-archive-row-select:checkbox').prop('checked', this.checked);  
+        let total= $('input[name="users[]"]:checked').length;
+        if( total > 0 ){
+            $('#users-archive-num-checked').text(`(${total})`);
+        }else{
+            $('#users-archive-num-checked').text('');
+        }
+    });
+
+    $(document).on('change', '.users-archive-row-select', function(){
+        let total= $('input[name="users[]"]:checked').length;
+        if( total > 0 ){
+            $('#users-archive-num-checked').text(`(${total})`);
+        }else{
+            $('#users-archive-num-checked').text('');
+        }
+    });
+});
+</script>
