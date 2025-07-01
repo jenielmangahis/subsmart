4ca2ea62e4d71f57c0cc0623878f460a5b640e85
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2025-07-01 16:58:12 +0800

    Fixes / Changes

--- application/views/v2/pages/more/ajax_load_plugin_details.php
+++ application/views/v2/pages/more/ajax_load_plugin_details.php
@@ -1,14 +1,15 @@
-<div class="row text-center gy-3">
+<input type="hidden" id="plugin-name" value="<?= $plugin->name; ?>" />
+<div class="row gy-3">
     <div class="col-12">
-        <label class="content-title d-block mb-2"><?= $plugin->name; ?></label>
+        <label class="content-title d-block mb-4" style="font-size:22px;font-weight:bold;"><i class='bx bxs-package' style="position:relative;top:2px;"></i> <?= $plugin->name; ?></label>
         <label class="content-subtitle d-block"><?= $plugin->description; ?></label>
         <hr>
-        <label class="nsm-subtitle text-success">
+        <label class="nsm-subtitle" style="font-size:20px;font-weight:bold;">
             <?php
             if ($plugin->sms_fee > 0) {
-                echo "$" . $plugin->sms_fee . "/SMS + $" . $plugin->service_fee . " service fee";
+                echo "$" . number_format($plugin->sms_fee,2,".",",") . "/ SMS + $" . $plugin->service_fee . " Service Fee";
             } else {
-                echo "$" . $plugin->service_fee . " service fee";
+                echo "$" . number_format($plugin->service_fee,2,".",",") . " Service Fee";
             }
             ?>
         </label>
