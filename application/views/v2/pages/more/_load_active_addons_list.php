d44c5e00b89fb03b9eabab02b43461a0b3359139
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2025-07-02 14:13:50 +0800

    Fix layout

--- application/views/v2/pages/more/_load_active_addons_list.php
+++ application/views/v2/pages/more/_load_active_addons_list.php
@@ -1,13 +1,7 @@
 <?php foreach ($NsmartUpgrades as $nu) : ?>
-    <?php if( in_array($nu->id, $active_addons_id)): ?>
+    <?php if( array_key_exists($nu->id, $active_addons_id)): ?>
         <div class="col-12 col-md-3">
-            <?php
-                $add_class = '';
-                if (in_array($nu->id, $active_addons_id)) {
-                    $add_class = 'availed';
-                }
-            ?>
-            <div class="nsm-card primary p-5 <?= $add_class; ?>" role="button" data-id="<?= $nu->id; ?>">
+            <div class="nsm-card primary p-5 role="button" data-id="<?= $nu->id; ?>">
                 <div class="nsm-card-content h-100">
                     <div class="row h-100 align-content-between">
                         <div class="col-12 text-center mb-3">
@@ -23,22 +17,24 @@
                         <div class="col-12 text-center">
                             <div class="row align-items-center mb-3">
                                 <div class="col-12">
-                                    <label class="nsm-subtitle text-success">
-                                        <?php
-                                        if ($nu->sms_fee > 0) {
-                                            echo "$" . $nu->sms_fee . "/SMS + $" . $nu->service_fee . " Service Fee";
-                                        } else {
-                                            echo "$" . $nu->service_fee . " Service Fee";
+                                    <label class="nsm-subtitle" style="font-size:15px;font-weight:bold;">
+                                    <?php  
+                                        if( $nu->sms_fee > 0 ){
+                                            //echo "$" . number_format($nu->sms_fee,2,".",",") . "/SMS + $" . $nu->service_fee . " Service Fee";
+                                            echo "$" . number_format($nu->service_fee,2,".",",") . " Service Fee";
+                                        }else{
+                                            echo "$" . number_format($nu->service_fee,2,".",",") . " Service Fee";
                                         }
-                                        ?>
+                                    ?>
                                     </label>
                                 </div>
                             </div>
-                            <?php if (!in_array($nu->id, $active_addons_id)) : ?>
-                                <button type="button" class="nsm-button">Subscribe Now</button>
-                            <?php else : ?>
-                                <button type="button" class="nsm-button primary">Active Addon</button>
-                            <?php endif; ?>
+                            <button type="button" class="nsm-button primary btn-open-addon" data-id="<?= $nu->id; ?>" data-name="<?= $nu->name; ?>">Open Module</button>
+                            <?php if( $active_addons_id[$nu->id] == 1 ){ ?>
+                                <button type="button" class="nsm-button btn-cancel-request-removal" data-tab="active-addons" data-id="<?= $nu->id; ?>" data-name="<?= $nu->name; ?>">Cancel Request Removal</button>
+                            <?php }else{ ?>
+                                <button type="button" class="nsm-button btn-remove-add-on" data-tab="active-addons" data-id="<?= $nu->id; ?>" data-name="<?= $nu->name; ?>">Remove Addon</button>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
