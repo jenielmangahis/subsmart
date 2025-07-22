ff2f7c9f19f1190b4396c4ecd49d0aa15ae0032d
Bryann Revina <bryann.revina03@gmail.com>
2025-07-22 12:33:38 +0800

    Fix error

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -72,9 +72,9 @@ class MY_Controller extends CI_Controller {
 
 	public function isSessionExpires()
 	{
-		$is_expired = false;
+		$is_expired = true;		
 		if( $this->session->userdata('login_token')){
-			$is_expired = true;
+			$is_expired = false;
 		}
 
 		return $is_expired;
