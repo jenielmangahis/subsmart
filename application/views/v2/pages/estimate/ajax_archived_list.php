718fde6c4c61ae0f9aecab6a32713b8f19d43546
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2025-08-04 22:52:33 +0800

    Fixes / Changes

--- application/views/v2/pages/estimate/ajax_archived_list.php
+++ application/views/v2/pages/estimate/ajax_archived_list.php
@@ -1,6 +1,26 @@
+<?php if(checkRoleCanAccessModule('estimates', 'write')){ ?>
+<div class="row">
+    <div class="col-12 grid-mb text-end">
+        <div class="dropdown d-inline-block">
+            <button type="button" class="nsm-button primary" id="btn-empty-archives">Empty Archived</button>
+            <button type="button" class="dropdown-toggle nsm-button" data-bs-toggle="dropdown">
+                <span id="estimates-archive-num-checked"></span> With Selected  <i class='bx bx-fw bx-chevron-down'></i>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end select-filter">                 
+                <li><a class="dropdown-item btn-with-selected" id="with-selected-restore" href="javascript:void(0);">Restore</a></li>                                
+                <li><a class="dropdown-item btn-with-selected" id="with-selected-perma-delete" href="javascript:void(0);">Permanently Delete</a></li>                                
+            </ul>
+        </div>
+    </div>
+</div>
+<?php } ?>
+<form id="frm-archive-with-selected">
 <table class="nsm-table" id="archived-estimates">
     <thead>
         <tr>
+            <td class="table-icon text-center sorting_disabled">
+                <input class="form-check-input table-select" type="checkbox" name="" value="0" id="estimates-archive-select-all">
+            </td>
             <td class="table-icon"></td>
             <td data-name="Name">Estimate Number</td>
             <td data-name="DateArchived">Date Archived</td>
@@ -11,6 +31,9 @@
         <?php if ($estimates) { ?>
             <?php foreach($estimates as $estimate){ ?>
                 <tr>
+                    <td class="text-center">
+                        <input class="form-check-input estimates-archive-row-select table-select" name="estimates[]" type="checkbox" value="<?= $estimate->id; ?>">
+                    </td>
                     <td><div class="table-row-icon"><i class="bx bx-receipt"></i></div></td>
                     <td class="nsm-text-primary"><?= $estimate->estimate_number; ?></td>
                     <td class="nsm-text-primary" style="width:25%;"><?= date("m/d/Y G:i A", strtotime($estimate->archived_date)); ?></td>
@@ -18,7 +41,8 @@
                         <div class="dropdown table-management">
                             <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown"><i class='bx bx-fw bx-dots-vertical-rounded'></i></a>
                             <ul class="dropdown-menu dropdown-menu-end">
-                                <li><a class="dropdown-item btn-restore-estimate" data-id="<?= $estimate->id; ?>" data-estimatenumber="<?= $estimate->estimate_number; ?>" href="javascript:void(0);"><i class='bx bx-recycle'></i> Restore</a></li>   
+                                <li><a class="dropdown-item btn-restore-estimate" data-id="<?= $estimate->id; ?>" data-estimatenumber="<?= $estimate->estimate_number; ?>" href="javascript:void(0);">Restore</a></li>   
+                                <li><a class="dropdown-item btn-permanently-delete-estimate" data-id="<?= $estimate->id; ?>" data-estimatenumber="<?= $estimate->estimate_number; ?>" href="javascript:void(0);">Permanently Delete</a></li>   
                             </ul>
                         </div>
                     </td>
@@ -26,7 +50,7 @@
             <?php } ?>
         <?php }else{ ?>
             <tr>
-                <td colspan="3">
+                <td colspan="5">
                     <div class="nsm-empty">
                         <span>No results found</span>
                     </div>
@@ -35,8 +59,28 @@
         <?php } ?>
     </tbody>
 </table>
+</form>
 <script>
 $(function(){
     $("#archived-estimates").nsmPagination();
+
+    $(document).on('change', '#estimates-archive-select-all', function(){
+        $('.estimates-archive-row-select:checkbox').prop('checked', this.checked);  
+        let total= $('#archived-estimates input[name="estimates[]"]:checked').length;
+        if( total > 0 ){
+            $('#estimates-archive-num-checked').text(`(${total})`);
+        }else{
+            $('#estimates-archive-num-checked').text('');
+        }
+    });
+
+    $(document).on('change', '.estimates-archive-row-select', function(){
+        let total= $('#archived-estimates input[name="estimates[]"]:checked').length;
+        if( total > 0 ){
+            $('#estimates-archive-num-checked').text(`(${total})`);
+        }else{
+            $('#estimates-archive-num-checked').text('');
+        }
+    });
 });
 </script>
