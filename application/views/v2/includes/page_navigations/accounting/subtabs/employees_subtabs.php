2bcf43b0e4a612305007e7201a569362f7ef6f81
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2025-08-25 21:59:52 +0800

    Changes

--- application/views/v2/includes/page_navigations/accounting/subtabs/employees_subtabs.php
+++ application/views/v2/includes/page_navigations/accounting/subtabs/employees_subtabs.php
@@ -11,10 +11,17 @@
             </a>
         </li>
         <?php if (logged('user_type') == 7) { ?>
-        <li class="<?php if($page->title == 'Payscale'): echo 'active'; endif; ?>" onclick="location.href='<?= base_url('users/pay_scale') ?>'">
-            <a class="nsm-page-link" href="javascript:void(0);">
-                <span>Payscale</span>
-            </a>
+        <li class="<?php if($page->title == 'Payscale' || $page->title == 'Job Titles'): echo 'active'; endif; ?>">
+            <div class="dropdown" id="test_dropdown">
+                <a class="nsm-page-link dropdown-toggle" role="button" href="javascript:void(0);">
+                    <span>Settings</span>
+                    <i class='bx bx-fw bx-chevron-down dropdown-icon'></i>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="<?php echo base_url('users/pay_scale') ?>">Payscale</a></li>
+                    <li><a class="dropdown-item" href="<?php echo base_url('users/job_titles') ?>">Job Title</a></li>
+                </ul>
+            </div>
         </li>
         <?php } ?>
     </ul>
