d5c3cd4b65e3b2a07c80ea5e458bbf39bae97915
Bryann Revina <bryann@theshiftleft.com>
2025-09-22 12:43:28 +0800

    Update files

--- application/views/v2/includes/accounting/products_and_services_modals.php
+++ application/views/v2/includes/accounting/products_and_services_modals.php
@@ -422,6 +422,20 @@
     </div>
 </div>
 
+<div class="modal fade nsm-modal fade" id="modal-archived-items" tabindex="-1" aria-labelledby="modal-archived-items-label" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <form method="post" id="archived-items-list-form">  
+                <div class="modal-header">
+                    <span class="modal-title content-title">Archived Inventory Items</span>
+                    <button type="button" data-bs-dismiss="modal" aria-label="Close"><i class='bx bx-fw bx-x m-0'></i></button>
+                </div>
+                <div class="modal-body" id="inventory-items-archived-list-container" style="max-height: 800px; overflow: auto;"></div>
+            </form>
+        </div>
+    </div>
+</div>
+
 <div id="overlay">
     <div>
         <img src="<?= base_url() ?>/assets/img/uploading.gif" class="" style="width: 80px;" alt="" />
