01d69d69e94c6318dd7bcd2bd5269cdecc351867
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2025-09-25 19:57:49 +0800

    Changes

--- application/views/v2/includes/page_navigations/workorder_subtabs.php
+++ application/views/v2/includes/page_navigations/workorder_subtabs.php
@@ -10,20 +10,20 @@
                 <span>Checklist</span>
             </a>
         </li>
-        <li class="<?php if($page->title == 'Workorder Priority'): echo 'active'; endif; ?>" onclick="location.href='<?= base_url('workorder/priority') ?>'">
+        <!-- <li class="<?php if($page->title == 'Workorder Priority'): echo 'active'; endif; ?>" onclick="location.href='<?= base_url('workorder/priority') ?>'">
             <a class="nsm-page-link" href="javascript:void(0);">
                 <span>Priority</span>
             </a>
-        </li>
+        </li> -->
         <li class="<?php if($page->title == 'Workorder Settings'): echo 'active'; endif; ?>" onclick="location.href='<?= base_url('workorder/settings') ?>'">
             <a class="nsm-page-link" href="javascript:void(0);">
                 <span>Settings</span>
             </a>
         </li>
-        <li class="<?php if($page->title == 'Workorder Status'): echo 'active'; endif; ?>" onclick="location.href='<?= base_url('workorder/status') ?>'">
+        <!-- <li class="<?php if($page->title == 'Workorder Status'): echo 'active'; endif; ?>" onclick="location.href='<?= base_url('workorder/status') ?>'">
             <a class="nsm-page-link" href="javascript:void(0);">
                 <span>Status</span>
             </a>
-        </li>
+        </li> -->
     </ul>
 </div>
