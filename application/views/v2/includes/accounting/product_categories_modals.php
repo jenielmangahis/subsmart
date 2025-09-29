1f3d6af6b8827c5078a0cbf5b46d484284cc3845
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2025-09-29 22:13:25 +0800

    Updates

--- application/views/v2/includes/accounting/product_categories_modals.php
+++ application/views/v2/includes/accounting/product_categories_modals.php
@@ -6,19 +6,23 @@
                     <span class="modal-title content-title product-category-modal-title" id="myModalLabel2">Category information</span>
                     <button type="button" data-bs-dismiss="modal" aria-label="Close"><i class='bx bx-fw bx-x m-0'></i></button>
                 </div>
-                <form action="<?=url('accounting/product-categories/create')?>" class="h-100" method="post" id="create-category-form">
+                <form class="h-100" method="post" id="create-category-form">
                     <div class="modal-body">
                         <div class="mb-2">
                             <label for="name">Name</label>
                             <input type="text" name="name" class="form-control nsm-field" required>
                         </div>
+                        <div class="mb-2">
+                            <label for="description">Description</label>
+                            <textarea class="form-control" name="description" style="height:100px;"></textarea>
+                        </div>
                         <div class="form-check mb-2">
                             <input type="checkbox" id="sub-category" value="1" class="form-check-input">
                             <label for="sub-category" class="form-check-label">Is a sub-category</label>
-                        </div>
+                        </div>                        
                     </div>
                     <div class="modal-footer bottom-0 position-absolute w-100">
-                        <button type="submit" class="nsm-button success">Save</button>
+                        <button type="submit" class="nsm-button success" id="btn-create-product-category">Save</button>
                     </div>
                 </form>
             </div>
