e5017175b92d3908bac074f1c9293e7e9cc46d49
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2025-10-13 17:31:43 +0800

    Updates

--- application/views/v2/includes/page_navigations/accounting/subtabs/products_and_services_subtabs.php
+++ application/views/v2/includes/page_navigations/accounting/subtabs/products_and_services_subtabs.php
@@ -2,18 +2,18 @@
     <ul>
         <li class="<?php if($page->title == 'Products and Services'): echo 'active'; endif; ?>" onclick="location.href='<?= base_url('accounting/products-and-services') ?>'">
             <a class="nsm-page-link" href="javascript:void(0);">
-                <span>Products and Services</span>
+                <span>List</span>
             </a>
         </li>
         <li class="<?php if($page->title == 'Product Categories'): echo 'active'; endif; ?>" onclick="location.href='<?= base_url('accounting/product-categories') ?>'">
             <a class="nsm-page-link" href="javascript:void(0);">
-                <span>Product Categories</span>
+                <span>Categories</span>
             </a>
         </li>
-        <li class="<?php if($page->title == 'All Lists'): echo 'active'; endif; ?>" onclick="location.href='<?= base_url('accounting/lists') ?>'">
+        <!-- <li class="<?php if($page->title == 'All Lists'): echo 'active'; endif; ?>" onclick="location.href='<?= base_url('accounting/lists') ?>'">
             <a class="nsm-page-link" href="javascript:void(0);">
                 <span>All Lists</span>
             </a>
-        </li>
+        </li> -->
     </ul>
 </div>
