bf1d02fb5d72b0d6f4a2a25599f7fb145418db3e
Bryann Revina <bryann.revina03@gmail.com>
2025-10-27 17:59:30 +0800

    Alam Zone model file

--- application/config/routes.php
+++ application/config/routes.php
@@ -2329,6 +2329,7 @@ $route['customer/_edit_alarm_zone'] = 'Customer/ajax_edit_alarm_zone';
 $route['customer/_update_alarm_zones'] = 'Customer/ajax_update_alarm_zones';
 $route['customer/_delete_selected_zones'] = 'Customer/ajax_delete_selected_zones';
 $route['customer/export_zones_list/(:any)?'] = 'Customer/export_zones_list/$1';
+$route['customer/_customer_emergency_agencies'] = 'Customer/ajax_customer_emergency_agencies/$1';
 
 //Benchmark for Testing
 $route['benchmark/test_customer_subscription'] = 'Benchmark/test_customer_subscription';
---
+++ application/models/AcsAlarmZone_model.php
@@ -0,0 +1,74 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class AcsAlarmZone_model extends MY_Model
+{
+    public $table = 'acs_alarm_zones';
+    
+    public function getAllByCompanyId($company_id)
+    {
+        $this->db->select('acs_alarm_zones.*, acs_profile.first_name, acs_profile.last_name');
+        $this->db->join('acs_profile', 'acs_alarm_zones.customer_id = acs_profile.prof_id', 'left');
+        $this->db->from($this->table);
+        $this->db->where('acs_profile.company_id', $company_id);
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getById($id)
+    {
+
+        $this->db->select('acs_alarm_zones.*, acs_profile.first_name, acs_profile.last_name');
+        $this->db->join('acs_profile', 'acs_alarm_zones.customer_id = acs_profile.prof_id', 'left');
+        $this->db->from($this->table);
+        $this->db->where('acs_alarm_zones.id', $id);
+
+        $query = $this->db->get();
+        return $query->row();
+    }
+
+    public function getAllByCustomerId($customer_id)
+    {
+        $this->db->select('acs_alarm_zones.*, acs_profile.first_name, acs_profile.last_name');
+        $this->db->join('acs_profile', 'acs_alarm_zones.customer_id = acs_profile.prof_id', 'left');
+        $this->db->from($this->table);
+        $this->db->where('acs_alarm_zones.customer_id', $customer_id);
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getByIdAndCompanyId($id, $cid)
+    {
+
+        $this->db->select('acs_alarm_zones.*, acs_profile.first_name, acs_profile.last_name');
+        $this->db->join('acs_profile', 'acs_alarm_zones.customer_id = acs_profile.prof_id', 'left');
+        $this->db->from($this->table);
+        $this->db->where('acs_alarm_zones.id', $id);
+        $this->db->where('acs_profile.company_id', $cid);
+
+        $query = $this->db->get();
+        return $query->row();
+    }
+
+    public function bulkDelete($ids = [], $filters = [])
+    {
+        if( count($ids) > 0 ){
+            $this->db->where_in('id', $ids);
+
+            if( $filters ){
+                foreach( $filters as $filter ){
+                    $this->db->where($filter['field'], $filter['value']);
+                }
+            }
+
+            $this->db->delete($this->table);
+        }        
+
+        return $this->db->affected_rows();
+    }
+}
+
+/* End of file AcsAlarmZone_model.php */
+/* Location: ./application/models/AcsAlarmZone_model.php */
