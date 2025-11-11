3bf6c1c84af55dc98915a3ef3519233116a9553b
Bryann Revina <bryann.revina03@gmail.com>
2025-11-11 21:13:11 +0800

    Changed title

--- application/views/v2/emails/customer_cancellation_request.php
+++ application/views/v2/emails/customer_cancellation_request.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Reset Password</title>
+    <title>Customer Cancellation Request</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.bunny.net/css?family=Nunito-sans" rel="stylesheet">
     <style>
